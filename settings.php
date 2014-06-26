6614b86a7a671b1eab2287f8818caa1717c58002
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-06-27 00:31:58 +0200

    Changed settings, everything is constant now.
    Sorry for breaking EVERYTHING

--- settings.php
+++ settings.php
@@ -1,37 +1,37 @@
 <?php
 class Settings {
 	// General.
-	public $name = 'Infected Crew';
-	public $description = 'Infected er et av Akershus største datatreff (LAN-party), og holder til i kulturhuset i Asker kommune.';
-	public $keywords = 'infected, lan, party, asker, kulturhus, ungdom, gaming';
-	public $authors = array('halvors', 'petterroea');
+	const name = 'Infected Crew';
+	const description = 'Infected er et av Akershus største datatreff (LAN-party), og holder til i kulturhuset i Asker kommune.';
+	const keywords = 'infected, lan, party, asker, kulturhus, ungdom, gaming';
+	const authors = array('halvors', 'petterroea');
 	
 	// Default page.
-	public $defaultPage = 'home';
+	const defaultPage = 'home';
 
 	// Database.
-	public $db_host = 'localhost';
+	const db_host = 'localhost';
 
-	public $db_name_infected = 'infecrjn_infected';
-	public $db_name_crew = 'infecrjn_crew';
+	const db_name_infected = 'infecrjn_infected';
+	const db_name_crew = 'infecrjn_crew';
 
-	public $db_prefixList = array('');
+	const db_prefixList = array('');
 
-	public $db_table_events = 'events';
-	public $db_table_pages = 'pages';
-	public $db_table_games = 'games';
-	public $db_table_gameapplications = 'gameapplications';
-	public $db_table_crews = 'crews';
-	public $db_table_users = 'users';
-	public $db_table_permissions = 'permissions';
-	public $db_table_agendas = 'agendas';
-	public $db_table_slides = 'slides';
-	public $db_table_teams = 'teams';
+	const db_table_events = 'events';
+	const db_table_pages = 'pages';
+	const db_table_games = 'games';
+	const db_table_gameapplications = 'gameapplications';
+	const db_table_crews = 'crews';
+	const db_table_users = 'users';
+	const db_table_permissions = 'permissions';
+	const db_table_agendas = 'agendas';
+	const db_table_slides = 'slides';
+	const db_table_teams = 'teams';
 	/*
-	public $host = 'localhost';
-	public $databaseList = array('infecrjn_infected', 'infecrjn_crew');
-	public $prefixList = array('');
-	public $tableList = array('events', 
+	const host = 'localhost';
+	const databaseList = array('infecrjn_infected', 'infecrjn_crew');
+	const prefixList = array('');
+	const tableList = array('events', 
 						'pages', 
 						'games', 
 						'gameapplications', 
@@ -44,8 +44,8 @@ class Settings {
 	*/
 	
 	// Create tables
-	public $tablesSql = array();
-	public $tablesDataSql = array();
+	const tablesSql = array();
+	const tablesDataSql = array();
 	
 	public function Settings() {
 		
