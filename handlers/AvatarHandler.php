5da20fdf579ac35f63128ba21cf171f647d2de3d
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-06-28 02:10:28 +0200

    Avatar handler

---
+++ handlers/AvatarHandler.php
@@ -0,0 +1,70 @@
+<?php
+require_once 'settings.php';
+require_once 'mysql.php';
+require_once 'avatar.php';
+
+class AvatarHandler {
+	/* Get a avatar by id */
+	public static function getAvatar($id) {
+		$con = MySQL::open(Settings::db_name_crew);
+		
+		$result = mysqli_query($con, 'SELECT * FROM ' . Settings::db_table_avatars . ' WHERE id=\'' . $id . '\'');
+		$row = mysqli_fetch_array($result);
+		
+		MySQL::close($con);
+
+		if ($row) {
+			return new Avatar($row['id'], 
+							$row['userId'], 
+							$row['relativeUrl'], 
+							$row['state']);
+		}
+	}
+	
+	public static function getAvatars() {
+		$con = MySQL::open(Settings::db_name_crew);
+		
+		$result = mysqli_query($con, 'SELECT id FROM ' . Settings::db_table_avatars);
+		
+		$avatarList = array();
+		
+		while ($row = mysqli_fetch_array($result)) {
+			array_push($groupList, self::getAvatar($row['id']));
+		}
+		
+		MySQL::close($con);
+		
+		return $avatarList;
+	}
+	
+	public static function getPendingAvatars() {
+		$con = MySQL::open(Settings::db_name_crew);
+		
+		$result = mysqli_query($con, 'SELECT id FROM ' . Settings::db_table_avatars . ' WHERE state=\'1\'');
+		
+		$avatarList = array();
+		
+		while ($row = mysqli_fetch_array($result)) {
+			array_push($groupList, self::getAvatar($row['id']));
+		}
+		
+		MySQL::close($con);
+		
+		return $avatarList;
+	}
+	
+	public static function getAvatarForUser($userId) {
+		$con = MySQL::open(Settings::db_name_crew);
+		
+		$result = mysqli_query($con, 'SELECT id FROM ' . Settings::db_table_avatars . ' WHERE userId=\'' . $userId . '\'');
+		$row = mysqli_fetch_array($result);
+		
+		MySQL::close($con);
+		
+		if ($row) {
+			return self::getAvatar($row['id']);
+		}
+	}
+}
+
+?>
