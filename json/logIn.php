698749254221485748dbe2015ce753da460addf1
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-07-03 21:52:56 +0200

    Semi-bugged login JSON

---
+++ json/logIn.php
@@ -0,0 +1,51 @@
+<?php
+
+require_once '/../utils.php';
+require_once '/../handlers/UserHandler.php';
+
+$result = true;
+$message = "";
+if(!Utils::isAuthenticated())
+{
+	if (isset($_POST['username']) &&
+			isset($_POST['password']) &&
+			!empty($_POST['username']) &&
+			!empty($_POST['password'])) 
+	{
+		$username = $_POST['username'];
+		$password = hash('sha256', $_POST['password']);
+
+		if (UserHandler::userExists($username)) 
+		{
+			$user = UserHandler::getUserByName($username);
+			$storedPassword = $user->getPassword();
+			
+			if ($password == $storedPassword) {
+				$_SESSION['user'] = $user;
+				$message = 'Du er nå logget inn!';
+			}
+			else
+			{
+				$result = false;
+				$message = 'Feil brukernavn eller passord.';
+			}
+		} 
+		else 
+		{
+			$result = false;
+			$message = 'Feil brukernavn eller passord.';
+		}
+	}
+	else
+	{
+		$result = false;
+		$message = "Du har ikke skrevet inn et brukernavn og passord.";
+	}
+}
+else
+{
+	$result = false;
+	$message = "Du er allerede logget inn!";
+}
+echo '{"result":' . ($result ? 'true' : 'false') . ', "message":"' . $message . '"}';
+?>
