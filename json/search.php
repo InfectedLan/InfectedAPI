e5a998ea794a394e8729010ac678f816cf2dac3e
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-07-18 00:55:13 +0200

    Fixed json for ticket site.

--- json/search.php
+++ json/search.php
@@ -1,31 +1,31 @@
 <?php
-	require_once '/../utils.php';
-	require_once '/../handlers/UserHandler.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/Utils.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/handlers/UserHandler.php';
 
-	if( !isset( $_GET["query"] ) || strlen( $_GET["query"] ) < 2)
-	{
-		echo '{"result":false}';
-		return;
-	}
+if( !isset( $_GET["query"] ) || strlen( $_GET["query"] ) < 2)
+{
+	echo '{"result":false}';
+	return;
+}
 
-	$list = UserHandler::searchUsers($_GET["query"]);
+$list = UserHandler::searchUsers($_GET["query"]);
 
-	if(!isset($list))
-	{
-		echo '{"result":false}';
-	}
-	else
-	{
-		echo '{"result":true, "key":"' . htmlspecialchars($_GET["key"]) . '", "users":[';
-		$first = true;
-		foreach ($list as $user) {
-			if($first==false)
-			{
-				echo ',';
-			}
-			echo '{"firstname":"' . $user->getFirstname() . '", "lastname":"' . $user->getLastname() . '", "nick":"' . $user->getNickname() . '"}';
-			$first = false;
+if(!isset($list))
+{
+	echo '{"result":false}';
+}
+else
+{
+	echo '{"result":true, "key":"' . htmlspecialchars($_GET["key"]) . '", "users":[';
+	$first = true;
+	foreach ($list as $user) {
+		if($first==false)
+		{
+			echo ',';
 		}
-		echo ']}';
+		echo '{"firstname":"' . $user->getFirstname() . '", "lastname":"' . $user->getLastname() . '", "nick":"' . $user->getNickname() . '"}';
+		$first = false;
 	}
+	echo ']}';
+}
 ?>
