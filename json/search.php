372c392db00b83e74ae527287f55cc606bc403fb
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-07-18 01:02:55 +0200

    Fixed a conflict in json/search.php.

--- json/search.php
+++ json/search.php
@@ -2,62 +2,34 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/Utils.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/api/handlers/UserHandler.php';
 
-<<<<<<< HEAD
-	if(Utils::isAuthenticated() == false)
-	{
-		echo '{"result":false, "message":"You arent authenticated!"}';
-		return;
-	}
+if (Utils::isAuthenticated() == false) {
+	echo '{"result":false, "message":"You arent authenticated!"}';
+	return;
+}
 
-	if( !isset( $_GET["query"] ) || strlen( $_GET["query"] ) < 2)
-	{
-		echo '{"result":false}';
-		return;
-	}
-=======
-if( !isset( $_GET["query"] ) || strlen( $_GET["query"] ) < 2)
-{
+if (!isset( $_GET["query"] ) || strlen( $_GET["query"] ) < 2) {
 	echo '{"result":false}';
 	return;
 }
->>>>>>> origin/master
 
 $list = UserHandler::searchUsers($_GET["query"]);
 
-<<<<<<< HEAD
-	if(!isset($list))
-	{
-		echo '{"result":false}';
-	}
-	else
-	{
-		echo '{"result":true, "key":"' . htmlspecialchars($_GET["key"]) . '", "users":[';
-		$first = true;
-		foreach ($list as $user) {
-			if($first==false)
-			{
-				echo ',';
-			}
-			echo '{"firstname":"' . $user->getFirstname() . '", "lastname":"' . $user->getLastname() . '", "nick":"' . $user->getNickname() . '", "userId":"' . $user->getId() . '"}';
-			$first = false;
-=======
-if(!isset($list))
-{
+if(!isset($list)) {
 	echo '{"result":false}';
-}
-else
-{
+} else {
 	echo '{"result":true, "key":"' . htmlspecialchars($_GET["key"]) . '", "users":[';
+	
 	$first = true;
+	
 	foreach ($list as $user) {
-		if($first==false)
-		{
+		if ($first==false) {
 			echo ',';
->>>>>>> origin/master
 		}
-		echo '{"firstname":"' . $user->getFirstname() . '", "lastname":"' . $user->getLastname() . '", "nick":"' . $user->getNickname() . '"}';
+		
+		echo '{"firstname":"' . $user->getFirstname() . '", "lastname":"' . $user->getLastname() . '", "nick":"' . $user->getNickname() . '", "userId":"' . $user->getId() . '"}';
 		$first = false;
 	}
+	
 	echo ']}';
 }
 ?>
