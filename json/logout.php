25d075aa8e44f7033a99b63fd812430832ad4fc8
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-07-18 04:00:47 +0200

    Fixed lowercase names.

--- json/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_destroy();
-
-echo '{"result":true}'; //I mean, how can this even fail?
-?>
--- secret.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
- * Store variables not to be given to users here
-*/
-class Secret {
-	const db_username = 'infectedlan.tk';
-	const db_password = 'c7xwrLP5f9n0P5dG';
-}
-?>
