2d4cf462229c4b8bc176f3b858007f5352d5739e
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-07-26 02:50:15 +0200

    Added CityDictionary.

---
+++ handlers/citydictionary.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'settings.php';
+require_once 'mysql.php';
+
+class CityDictionary {
+	/* 
+	 * Returns the city from given postalcode.
+	 */
+	public static function getCity($postalcode) {
+		$con = MySQL::open(Settings::db_name_infected);
+		
+		$result = mysqli_query($con, 'SELECT `city`
+									  FROM `' . Settings::db_table_infected_postalcodes . '`
+									  WHERE `code` = \'' . $postalcode . '\';');
+									  
+		$row = mysqli_fetch_array($result);
+		
+		MySQL::close($con);
+		
+		if ($row) {
+			return ucfirst(strtolower($row['city']));
+		}
+	}
+	
+	/*
+	 * Returns the postalcode for given city.
+	 */
+	public static function getPostalCode($city) {
+		$con = MySQL::open(Settings::db_name_infected);
+		
+		$result = mysqli_query($con, 'SELECT `code` 
+								      FROM `' . Settings::db_table_infected_postalcodes . '` 
+									  WHERE `city` = \'' . $city . '\';');
+		
+		$row = mysqli_fetch_array($result);
+		
+		MySQL::close($con);
+		
+		if ($row) {
+			return ucfirst(strtolower($row['city']));
+		}
+	}
+}
+?>
