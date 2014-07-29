8e0790f731fb296f715d7aceec24f16387d25b2a
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-07-29 02:24:54 +0200

    Added isMember() function that returns true if the current user is a crew member. Similar to the isAuthenticated() except that this could return false even if isAuthenticated() is true.

--- session.php
+++ session.php
@@ -3,13 +3,31 @@ require_once 'objects/user.php';
 
 session_start();
 
+/*
+ * Used to get information from current sessions.
+ */
 class Session {
+	/*
+	 * Returns true if the current user is authenticated.
+	 */
 	public static function isAuthenticated() {
 		return isset($_SESSION['user']);
 	}
 	
+	/*
+	 * Returns true if the current user is a member (To clarify, is a crew member).
+	 */
+	public static function isMember() {
+		return self::getCurrentUser()->isGroupMember();
+	}
+	
+	/*
+	 * Returns the current user.
+	 */
 	public static function getCurrentUser() {
-		return self::isAuthenticated() ? $_SESSION['user'] : null;
+		if (self::isAuthenticated()) {
+			return $_SESSION['user'];
+		}
 	}
 }
 ?>
