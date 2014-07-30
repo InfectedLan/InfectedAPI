c018fefe818d58f497014b7ab642883eeeb1bb65
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-07-30 03:00:26 +0200

    You can now create new seatmaps

--- json/newSeatmap.php
+++ json/newSeatmap.php
@@ -1,13 +1,43 @@
 <?php
 	require_once 'session.php';
-	require_once 'handlers/tickethandler.php';
+	require_once 'handlers/seatmaphandler.php';
 
 	$result = false;
 	$message = null;
+	$id = null;
 
 	if (Session::isAuthenticated()) {
 		$user = Session::getCurrentUser();
-		
+		if ($user->hasPermission('admin.seatmap') ||
+			$user->hasPermission('admin')) {
+			if(isset($_GET["name"]))
+			{
+				$seatmap = SeatmapHandler::createNewSeatmap($_GET["name"]);
+				$result = true;
+				$id = $seatmap->getId();
+			}
+			else
+			{
+				$message = "Navn er ikke satt!";
+			}
+		}
+		else
+		{
+			$message = "Du har ikke tillatelse til å lage et seatmap!";
+		}
+	}
+	else
+	{
+		$message = "Du må logge inn først!";
+	}
+
+	if($result)
+	{
+		echo json_encode(array('result' => $result, 'id' => $id));
+	}
+	else
+	{
+		echo json_encode(array('result' => $result, 'message' => $message));
 	}
 
 ?>
