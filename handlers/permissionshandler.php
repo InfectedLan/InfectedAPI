2262c092ff17fe34e9ef869657dccbbd2974460b
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-01 18:31:11 +0200

    Added add, remove and get permission functions to PermissionsHandler.

--- handlers/permissionshandler.php
+++ handlers/permissionshandler.php
@@ -7,8 +7,7 @@ class PermissionsHandler {
 	public static function hasPermission($userId, $permission) {
 		$con = MySQL::open(Settings::db_name_infected);
 		
-		$result = mysqli_query($con, 'SELECT `value` 
-									  FROM `' . Settings::db_table_infected_permissions . '` 
+		$result = mysqli_query($con, 'SELECT `value` FROM `' . Settings::db_table_infected_permissions . '` 
 									  WHERE `userId` = \'' . $userId . '\' 
 									  AND `value` = \'' . $permission . '\';');
 								
@@ -17,6 +16,43 @@ class PermissionsHandler {
 		MySQL::close($con);
 		
 		return $row ? true : false;
+	}
+	
+	public static function addPermission($userId, $permission) {
+		$con = MySQL::open(Settings::db_name_infected);
+		
+		mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_permissions . '` (`userId`, `value`) 
+							VALUES (\'' . $userId . '\', 
+									\'' . $permission . '\')');
+		
+		MySQL::close($con);
+	}
+	
+	public static function removePermission($userId, $permission) {
+		$con = MySQL::open(Settings::db_name_infected);
+		
+		mysqli_query($con, 'DELETE FROM `' . Settings::db_table_infected_permissions . '` 
+							WHERE `userId` = \'' . $userId . '\'
+							AND `value` = \'' . $permission . '\';');
+		
+		MySQL::close($con);
+	}
+	
+	public static function getPermissions($userId) {
+		$con = MySQL::open(Settings::db_name_infected);
+		
+		$result = mysqli_query($con, 'SELECT `id` FROM `' . Settings::db_table_infected_permissions . '`
+									  WHERE `userId` = \'' . $userId . '\';');
+		
+		$permissionList = array();
+		
+		while ($row = mysqli_fetch_array($result)) {
+			array_push($pageList, $row['value']);
+		}
+		
+		MySQL::close($con);
+
+		return $permissionList;
 	}
 }
 ?>
