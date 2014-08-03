a331a666b50f11000a7257f0c9fdbaaf87842bf6
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-08-03 03:07:16 +0200

    Added json to add seats to a row

---
+++ json/rowAddSeats.php
@@ -0,0 +1,68 @@
+<?php
+	require_once 'session.php';
+	require_once 'handlers/rowhandler.php';
+	require_once 'handlers/seatmaphandler.php';
+
+	$result = false;
+	$message = null;
+	$id = null;
+
+	if (Session::isAuthenticated()) {
+		$user = Session::getCurrentUser();
+		if ($user->hasPermission('admin.seatmap') ||
+			$user->hasPermission('admin')) {
+			if(isset($_GET["row"]))
+			{
+				$row = RowHandler::getRow($_GET["row"]);
+				if(isset($row))
+				{
+					if(isset($_GET["numSeats"]))
+					{
+						$seats = $_GET["numSeats"];
+						if(is_numeric($seats))
+						{
+							for($i = 0; $i < $seats; $i++)
+							{
+								RowHandler::addSeat($row);
+							}
+							$result = true;
+						}
+						else
+						{
+							$message = "Antall seter er ikke et tall!";
+						}
+					}
+					else
+					{
+						$message = "Antall seter er ikke satt!";
+					}
+				}
+				else
+				{
+					$message = "Raden eksisterer ikke!";
+				}
+			}
+			else
+			{
+				$message = "Raden er ikke satt!";
+			}
+		}
+		else
+		{
+			$message = "Du har ikke tillatelse til å legge til en rad!";
+		}
+	}
+	else
+	{
+		$message = "Du må logge inn først!";
+	}
+
+	if($result)
+	{
+		echo json_encode(array('result' => $result));
+	}
+	else
+	{
+		echo json_encode(array('result' => $result, 'message' => $message));
+	}
+?>
