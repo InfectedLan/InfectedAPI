de04104c527e82031808815999f13c9fea615e99
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-13 09:22:30 +0200

    Readded lowercase storesession object.

---
+++ objects/storesession.php
@@ -0,0 +1,62 @@
+<?php
+class StoreSession {
+	private $id;
+	private $userId;
+	private $timeCreated;
+	private $ticketType;
+	private $amount;
+
+	/*
+	 * StoreSession - represents a session in the ticket shop.
+	 * 
+	 * This is used to reserve a ticket for someone after they pressed "buy", until the payment has been processed.
+	 * I didnt name it session because that would be confusing.
+	 * 
+	 * Id: Unique id of seat
+	 * UserId: ID of user connected to the session
+	 * TimeCreated: time this session was created, used for calculating if session has timed out
+	 */
+	public function __construct($id, $userId, $timeCreated, $ticketType, $amount) {
+		$this->id = $id;
+		$this->userId = $userId;
+		$this->timeCreated = $timeCreated;
+		$this->ticketType = $ticketType;
+		$this->amount = $amount;
+	}
+
+	/*
+	 * Returns unique id of this session
+	 */
+	public function getId() {
+		return $this->id;
+	}
+
+	/*
+	 * Returns the user connected to this session
+	 */
+	public function getUserId() {
+		return $this->userId;
+	}
+
+	/*
+	 * Returns the time this session was created
+	 */
+	public function getTimeCreated() {
+		return $this->timeCreated;
+	}
+
+	/*
+	 * Returns the ticket type the user is buying
+	 */
+	public function getTicketType() {
+		return $this->ticketType;
+	}
+
+	/*
+	 * Returns the amount of tickets the user is buying
+	 */
+	public function getAmount() {
+		return $this->amount;
+	}
+}
+?>
