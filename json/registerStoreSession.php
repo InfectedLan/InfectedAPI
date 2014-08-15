5d8f375f906b9036093c1f3530a33119d605018d
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-15 18:47:48 +0200

    Some fixes

--- json/registerStoreSession.php
+++ json/registerStoreSession.php
@@ -1,39 +1,36 @@
 <?php
-	require_once 'session.php';
-	require_once 'handlers/storesessionhandler.php';
+require_once 'session.php';
+require_once 'handlers/storesessionhandler.php';
 
-	$result = false;
-	$message = null;
-	$key = null;
+$result = false;
+$message = null;
+$key = null;
 
-	if (Session::isAuthenticated()) {
-		$user = Session::getCurrentUser();
-		if (isset($_GET['ticketType']) &&
-			isset($_GET['amount'])) {		
-			$type = $_GET['ticketType'];
-			$amount = $_GET['amount'];
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+	if (isset($_GET['ticketType']) &&
+		isset($_GET['amount'])) {		
+		$type = $_GET['ticketType'];
+		$amount = $_GET['amount'];
 
-			if(!StoreSessionHandler::hasStoreSession($user)) {
-				$key = StoreSessionHandler::registerStoreSession($user, TicketTypeHandler::getTicketType($type), $amount);
+		if(!StoreSessionHandler::hasStoreSession($user)) {
+			$key = StoreSessionHandler::registerStoreSession($user, TicketTypeHandler::getTicketType($type), $amount);
 
-				$result = true;
-				$message = 'Hi mom!';
-			} else {
-				$message = "Du har allerede en session!";
-			}
+			$result = true;
+			$message = 'Hi mom!';
 		} else {
-			$message = 'Du har ikke fyllt ut alle feltene.';
+			$message = "Du har allerede en session!";
 		}
 	} else {
-		$message = "Du er ikke logget inn!";
-	} 
+		$message = 'Du har ikke fyllt ut alle feltene.';
+	}
+} else {
+	$message = "Du er ikke logget inn!";
+} 
 
-	if($result)
-	{
-		echo json_encode(array('result' => $result, 'key' => $key));
-	}
-	else
-	{
-		echo json_encode(array('result' => $result, 'message' => $message));
-	}
+if ($result) {
+	echo json_encode(array('result' => $result, 'key' => $key));
+} else {
+	echo json_encode(array('result' => $result, 'message' => $message));
+}
 ?>
