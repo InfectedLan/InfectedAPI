44fa5a6db8ec12dbb0fd291d35e84879339954a5
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-15 13:04:18 +0200

    Added changeuser

---
+++ json/changeToUser.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'session.php';
+require_once 'handlers/userhandler.php';
+
+$result = false;
+$message = null;
+
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+	
+	if ($user->hasPermission('admin') ||
+		$user->hasPermission('admin.changeuser')) {
+		if (isset($_GET['userId']) &&
+			is_numeric($_GET['userId'])) {
+			$changeUser = UserHandler::getUser($_GET['userId']);
+
+			if ($changeUser != null) {
+				$_SESSION['user'] = $changeUser;
+				$result = true;
+			} else {
+				$message = '<p>Brukeren du prøvde å bytte til eksisterer ikke.</p>';
+			}
+		} else {
+			$message = 'Du har ikke fyllt ut alle feltene.';
+		}
+	} else {
+		$message = 'Du har ikke rettigheter til dette.';
+	}
+} else {
+	$message = "Du er allerede logget inn!";
+} 
+
+echo json_encode(array('result' => $result, 'message' => $message));
+?>
