4bef741987736a9d96ce4ba18efc359d4dccb2fe
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-16 02:07:05 +0200

    Fixed conflicts

--- json/getPaypalUrl.php
+++ json/getPaypalUrl.php
@@ -1,49 +1,46 @@
 <?php
-	require_once 'session.php';
-	require_once 'handlers/storesessionhandler.php';
-	require_once 'handlers/userhandler.php';
-	require_once 'paypal/paypal.php';
+require_once 'session.php';
+require_once 'handlers/storesessionhandler.php';
+require_once 'handlers/userhandler.php';
+require_once 'paypal/paypal.php';
 
-	$result = false;
-	$message = null;
-	$url = null;
+$result = false;
+$message = null;
+$url = null;
 
-	if (Session::isAuthenticated()) {
-		$user = Session::getCurrentUser();
-		if (isset($_GET['ticketType']) &&
-			isset($_GET['amount'])) {		
-			$type = $_GET['ticketType'];
-			$amount = $_GET['amount'];
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+	if (isset($_GET['ticketType']) &&
+		isset($_GET['amount'])) {		
+		$type = $_GET['ticketType'];
+		$amount = $_GET['amount'];
 
-			if(!StoreSessionHandler::hasStoreSession($user)) {
-				$ticketType = TicketTypeHandler::getTicketType($type);
-				//Register store session
-				$key = StoreSessionHandler::registerStoreSession($user, $ticketType, $amount);
+		if(!StoreSessionHandler::hasStoreSession($user)) {
+			$ticketType = TicketTypeHandler::getTicketType($type);
+			//Register store session
+			$code = StoreSessionHandler::registerStoreSession($user, $ticketType, $amount);
 
-				$url = PayPal::getPaymentUrl($ticketType, $amount, $key, $user);
+			$url = PayPal::getPaymentUrl($ticketType, $amount, $code, $user);
 
-				if( isset($url) ) {
-					$result = true;
-				} else {
-					$message = "Noe gikk galt da vi snakket med paypal";
-				}
-
+			if (isset($url)) {
+				$result = true;
 			} else {
-				$message = "Du har allerede en session!";
+				$message = "Noe gikk galt da vi snakket med paypal";
 			}
+
 		} else {
-			$message = 'Du har ikke fyllt ut alle feltene.';
+			$message = "Du har allerede en session!";
 		}
 	} else {
-		$message = "Du er ikke logget inn!";
-	} 
+		$message = 'Du har ikke fyllt ut alle feltene.';
+	}
+} else {
+	$message = "Du er ikke logget inn!";
+} 
 
-	if($result)
-	{
-		echo json_encode(array('result' => $result, 'url' => $url));
-	}
-	else
-	{
-		echo json_encode(array('result' => $result, 'message' => $message));
-	}
+if ($result) {
+	echo json_encode(array('result' => $result, 'url' => $url));
+} else {
+	echo json_encode(array('result' => $result, 'message' => $message));
+}
 ?>
