c6004973f4c7228fbf7ec49e20821fad1ff53597
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-17 20:07:04 +0200

    Fixed typo

--- json/searchForUser.php
+++ json/searchForUser.php
@@ -9,7 +9,7 @@ if (Session::isAuthenticated()) {
 	$user = Session::getCurrentUser();
 
 	if ($user->hasPermission('*') ||
-		$user->hasPermission('functions.find-user')) {
+		$user->hasPermission('functions.find-users')) {
 		if (isset($_GET['query']) &&
 			strlen($_GET['query']) >= 2) {
 			$query = preg_replace('/[^A-Za-z0-9]/', ' ', $_GET['query']);
