a13b36963f1da3eb69a4e62d9d3748f8f51ec74b
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-08-18 09:11:09 +0200

    ...attempt at fixing QR codes. Still doesnt work. I am sad.

--- qr.php
+++ qr.php
@@ -3,9 +3,10 @@ require_once 'phpqrcode/qrlib.php';
 require_once 'settings.php';
 
 class QR {
-	public function getCode($content) {
+	public static function getCode($content) {
 		$fileName = md5($content) . '.png';
-		$filePath = Settings::qr_path . $fileName;
+		//$filePath = Settings::qr_path . $fileName;
+		$filePath = 'content/seatmapBackground/' . $fileName;
     
 		if (!file_exists($filePath)) {
 			QRcode::png($content, $filePath);
