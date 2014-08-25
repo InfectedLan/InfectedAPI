a3f8cc62e012bb621116898db3897e308a958993
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-08-25 13:41:23 +0200

    Updates to logging

--- handlers/paymentloghandler.php
+++ handlers/paymentloghandler.php
@@ -5,24 +5,19 @@ require_once 'mysql.php';
 class PaymentLogHandler
 {
 	//Couldnt be bothered to write object for this yet, as we only log for security purposes
-	public static function logPayment($user, $item_name, $item_number, $payment_status, $payment_amount, $payment_currency, $txn_id, $receiver_email, $payer_email, $quantity)
+	public static function logPayment($user, $ticketType, $amount, $total, $transactionId)
 	{
 		$time = time();
 
 		$con = MySQL::open(Settings::db_name_infected_tickets);
 
-		mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_tickets_paymentlog . '` (`timeLogged`, `itemName`, `itemNumber`, `paymentStatus`, `paymentAmount`, `paymentCurrency`, `txnId`, `recieverEmail`, `payerEmail`, `quantity`, `userId`) 
+		mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_tickets_paymentlog . '` (`timeLogged`, `userId`, `ticketType`, `amount`, `totalPrice`, `transactionId`) 
 							VALUES (' . $con->real_escape_string($time) . ', 
-									\'' . $con->real_escape_string($item_name) . '\', 
-									\'' . $con->real_escape_string($item_number) . '\',
-									\'' . $con->real_escape_string($payment_status) . '\', 
-									\'' . $con->real_escape_string($payment_amount) . '\', 
-									\'' . $con->real_escape_string($payment_currency) . '\', 
-									\'' . $con->real_escape_string($txn_id) . '\', 
-									\'' . $con->real_escape_string($receiver_email) . '\',
-									\'' . $con->real_escape_string($payer_email) . '\', 
-									\'' . $con->real_escape_string($quantity) . '\', 
-									' . $con->real_escape_string($user->getId()) . ');');
+									\'' . $con->real_escape_string($user->getId()) . '\', 
+									\'' . $con->real_escape_string($ticketType->getId()) . '\',
+									\'' . $con->real_escape_string($amount) . '\', 
+									\'' . $con->real_escape_string($total) . '\', 
+									\'' . $con->real_escape_string($transactionId) . ');');
 
 		MySQL::close($con);
 	}
