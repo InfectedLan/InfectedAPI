480f9fb556b7e0ebffd270a20c7626c1c4835b24
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-08-27 22:00:00 +0200

    Warbo pirk

--- scripts/register.js
+++ scripts/register.js
@@ -3,7 +3,8 @@
 		e.preventDefault();
 		$.getJSON('../api/json/register.php' + '?' + $(this).serialize(), function(data){
 			if (data.result) {
-				$(location).attr('href', 'index.php');
+				//$(location).attr('href', 'index.php');
+				info('Du har blitt registrert, og en e-post har blitt sendt til addressen du har oppgitt for å kunne aktivere brukeren.');
 			} else {
 				error(data.message); 
 			}
