3fe885316c721b7f972945f0f3b08d6eaa96bd75
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-08-27 15:44:12 +0200

    Cleanup in settings

--- settings.php
+++ settings.php
@@ -1,20 +1,12 @@
 <?php
 class Settings {
-	// Information
+	/* Metadata */
 	const name = 'Infected';
 	const description = 'Infected er et av Akershus største datatreff (LAN-party), og holder til i kulturhuset i Asker kommune.';
 	const keywords = 'infected, lan, party, asker, kulturhus, ungdom, gaming';
 	public static $authors = array('halvors', 'petterroea');
 	
-	const api_path = '/home/test.infected.no/public_html/api/';
-	const qr_path = 'content/qrcache/';
-	
-	const emailName = 'Infected';
-	const email = 'no-reply@infected.no';
-
-	const storeSessionTime = 3600; //You have an hour to pay on paypal
-	
-	// Database information
+	/* Database */
 	const db_host = 'localhost';
 	const db_name_infected = 'test_infected_no';
 	const db_name_infected_main = 'test_infected_no_main';
@@ -56,5 +48,36 @@ class Settings {
 	const db_table_infected_tickets_storesessions = 'storesessions';
 	const db_table_infected_tickets_tickets = 'tickets';
 	const db_table_infected_tickets_tickettypes = 'tickettypes';
+	
+	/* Permissions */
+    public static $permissions = array('*' 				    	   => 'Gir tilgang til absolutt alt, du er nå en udødelig administrator.',
+									   'functions.search-users'    => 'Søk etter brukere i databasen.',
+									   'functions.my-crew' 		   => 'Endre sider under "My Crew".',
+									   'functions.info'			   => 'Post informasjon på infoskjermen som vil vises under arrangementet.',
+									   'functions.site-list-games' => 'Administrer spill, settes opp compo i nye spill og moderer påmeldinger.',
+									   'functions.site-list-pages' => 'Endre innholdet på nettsiden.',
+									   'chief.home' 			   => 'Endre "home" siden."',
+									   'chief.groups' 			   => 'Administrer crewene, medlemmene i dem og hvem som er ledere.',
+									   'chief.teams' 			   => 'Administrer lagene for ditt crew, hvem som er medlem av hvilke lag og lederene i lagene.',
+									   'chief.avatars'			   => 'Administrer profilbilder, alle nye profilbilder vil dukke opp her og du kan godjkenne dem eller avise.',
+									   'chief.applications'		   => 'Godta eller avvis crew-søknader.',
+									   'admin.events' 			   => 'Administrer arrangementer, informasjon er blir automatisk endret på hovedsiden og ticketsiden.',
+									   'admin.permissions' 		   => 'Velg hvilke bruker som skal ha tilgang til hva, dette er et tilgangsystem, men husk at brukere har standard tilganger utenom dette, ettersom de er medlem av crewet eller ikke.',
+									   'admin.change-user' 		   => 'Lar deg logge inn som hvilken som helst annen bruker, kun beregnet for bruk ved feilsøking.',
+									   'admin.seatmap' 			   => 'Endre seatmappet, her kan du flytte, legg til, og fjerne seter og rader.');
+	
+	/* Configuration */
+	// Full path to the API location.
+	const api_path = '/home/test.infected.no/public_html/api/';
+	
+	// Email information
+	const emailName = 'Infected';
+	const email = 'no-reply@infected.no';
+	
+	// Tells where QR images should be stored under API folder.
+	const qr_path = 'content/qrcache/';
+
+	// Defines how long a ticket should be stored on your account before payment is successful.
+	const storeSessionTime = 3600; // You have an hour to pay on paypal
 }
 ?>
