9a286195477bfd4752d73a5588cd7a7664e130b0
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-09-01 15:33:29 +0200

    Added removeUsersFromTeam to json api

---
+++ json/removeUsersFromTeam.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'session.php';
+require_once 'handlers/teamhandler.php';
+
+$result = false;
+$message = null;
+
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+	
+	if ($user->hasPermission('*') ||
+		$user->hasPermission('chief.teams') ||
+		$user->isGroupLeader()) {
+		if (isset($_GET['id']) &&
+			is_numeric($_GET['id'])) {
+			$team = TeamHandler::getTeam($_GET['id']);
+			$memberList = $team->getMembers();
+			
+			foreach ($memberList as $member) {
+				TeamHandler::removeUserFromTeam($member);
+			}
+			
+			$result = true;
+		} else {
+			$message = 'Ikke noe lag spesifisert.';
+		}
+	} else {
+		$message = 'Du har ikke tillatelse til dette.';
+	}
+} else {
+	$message = 'Du er ikke logget inn.';
+}
+
+echo json_encode(array('result' => $result, 'message' => $message));
+?>
