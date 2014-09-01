0dafe296539a5e682976eaedc19034f5e76f39be
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-09-01 11:16:38 +0200

    Fixed activation

--- pages/activation.php
+++ pages/activation.php
@@ -2,10 +2,22 @@
 require_once 'handlers/registrationcodehandler.php';
 
 if (isset($_GET['code'])) {
-	RegistrationCodeHandler::removeRegistrationCode($_GET['code']);
-	
-	echo 'Brukeren din er nå aktivert og klar for bruk.';
+	$code = $_GET['code'];
+
+	if (RegistrationCodeHandler::hasRegistrationCode($code)) {
+		RegistrationCodeHandler::removeRegistrationCode($_GET['code']);
+		
+		echo '<li>';
+			echo '<p>Brukeren din er nå aktivert og klar for bruk.</p>';
+		echo '</li>';
+	} else {
+		echo '<li>';
+			echo '<p>Brukeren din er allerede aktivert.</p>';
+		echo '</li>';
+	}
 } else {
-	echo 'Brukeren din er allerede aktivert.';
+	echo '<li>';
+		echo '<p>En feil oppstod.</p>';
+	echo '</li>';
 }
 ?>
