b01129c0ca7b39c9afae45db8e8b8000ae3cf18a
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-09-01 15:27:22 +0200

    Added removeUsersFromGroup to json api

---
+++ json/removeUsersFromGroup.php
@@ -0,0 +1,35 @@
+<?php
+require_once 'session.php';
+require_once 'handlers/grouphandler.php';
+
+$result = false;
+$message = null;
+
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+	
+	if ($user->hasPermission('*') ||
+		$user->hasPermission('chief.groups') ||
+		$user->isGroupLeader()) {
+		if (isset($_GET['id']) &&
+			is_numeric($_GET['id'])) {
+			$group = GroupHandler::getGroup($_GET['id']);
+			$memberList = $group->getMembers();
+			
+			foreach ($memberList as $member) {
+				GroupHandler::removeUserFromGroup($member);
+			}
+			
+			$result = true;
+		} else {
+			$message = 'Ingen gruppe spesifisert.';
+		}
+	} else {
+		$message = 'Du har ikke tillatelse til dette.';
+	}
+} else {
+	$message = 'Du er ikke logget inn.';
+}
+
+echo json_encode(array('result' => $result, 'message' => $message));
+?>
