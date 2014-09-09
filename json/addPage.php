63d5bac835d6ba2753fcf72d2eb5f6f7b8ff2a68
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-09-09 15:46:36 +0200

    Added json page handlers

---
+++ json/addPage.php
@@ -0,0 +1,36 @@
+<?php
+require_once 'session.php';
+require_once 'handlers/pagehandler.php';
+
+$result = false;
+$message = null;
+
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+	
+	if ($user->hasPermission('*') ||
+		$user->hasPermission('functions.edit-page') || 
+		$user->hasPermission('functions.site-list-pages')) {
+		if (isset($_GET['title']) &&
+			isset($_GET['content']) &&
+			!empty($_GET['title']) &&
+			!empty($_GET['content'])) {
+			$name = strtolower(str_replace(' ', '-', $_GET['title']));
+			$title = $_GET['title'];
+			$content = $_GET['content'];
+			
+			PageHandler::createPage($name, $title, $content);
+			$result = true;
+		} else {
+			$message = 'Du har ikke fyllt ut alle feltene!';
+		}
+	} else {
+		$message = 'Du har ikke tillatelse til dette.';
+	}
+} else {
+	$message = 'Du er ikke logget inn.';
+}
+
+echo json_encode(array('result' => $result, 'message' => $message));
+
+?>
---
+++ json/removePage.php
@@ -0,0 +1,29 @@
+<?php
+require_once 'session.php';
+require_once 'handlers/pagehandler.php';
+
+$result = false;
+$message = null;
+
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+	
+	if ($user->hasPermission('*') ||
+		$user->hasPermission('functions.edit-page') || 
+		$user->hasPermission('functions.site-list-pages')) {
+		if (isset($_GET['id']) &&
+			is_numeric($_GET['id'])) {
+			PageHandler::removePage($_GET['id']);
+			$result = true;
+		} else {
+			$message = 'Ikke noen side spesifisert.';
+		}
+	} else {
+		$message = 'Du har ikke tillatelse til dette.';
+	}
+} else {
+	$message = 'Du er ikke logget inn.';
+}
+
+echo json_encode(array('result' => $result, 'message' => $message));
+?>
