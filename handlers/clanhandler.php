dbe31ed458403098bfa5dbc01f8e1d55394ded51
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-09-12 13:11:44 +0200

    Warbo wanted change. He gets change

--- handlers/clanhandler.php
+++ handlers/clanhandler.php
@@ -29,28 +29,5 @@ class ClanHandler {
 		
 		MySQL::close($con);
 	}
-
-
-	public static function inviteUser($user, $clan)
-	{
-		$con = MySQL::open(Settings::db_name_infected_compo);
-
-		$inviteKey = md5(time() . ' ' . $user->getId() . $clan->getId());
-
-		mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_compo_invites . '` (`userId`, `clanId`, `inviteCode`) VALUES (\'' . $user->getId() . '\', \'' . $clan->getId() . '\', \'' . $inviteKey . '\');');
-
-		//Generate mail
-		$message = array();
-		$message[] = '<!DOCTYPE html>';
-		$message[] = '<html>';
-			$message[] = '<body>';
-				$message[] = '<h3>Hei!</h3>';
-				$message[] = '<p>Du har blitt invitert i klanen <b>' . $clan->getName() . '</b> av ' . $user->getFullName() . '. Dersom du ønsker å være med, <a href="https://compo.infected.no/index.php?page=acceptClan&code=' . $inviteKey . '">klikk her</a>.</p>';
-				$message[] = '<p>Med vennlig hilsen <a href="http://infected.no/">Infected</a>.</p>';
-			$message[] = '</body>';
-		$message[] = '</html>';
-			
-		return MailManager::sendMail($user, 'Du har blitt invitert til en klan!', implode("\r\n", $message));
-	}
 }
 ?>
