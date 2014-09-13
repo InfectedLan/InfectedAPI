ab8e5362f5f26303da91ce8a8364943e1d35d79d
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-09-14 00:21:40 +0200

    Implementet accepting and declining invites

--- objects/invite.php
+++ objects/invite.php
@@ -1,4 +1,6 @@
 <?php
+require_once 'mysql.php';
+require_once 'settings.php';
 class Invite {
 	private $id;
 	private $userId;
@@ -20,6 +22,24 @@ class Invite {
 
 	public function getClanId() {
 		return $this->clanId;
+	}
+
+	public function decline() {
+		$con = MySQL::open(Setttings::db_name_infected_compo);
+
+		mysqli_query($con, 'DELETE FROM `' . Settings::db_table_infected_compo_invites . '` WHERE `id` = ' . $this->id . ';');
+	
+		MySQL::close($con);
+	}
+
+	public function accept() {
+		$con = MySQL::open(Settings::db_name_infected_compo);
+
+		mysqli_query($con, 'DELETE FROM `' . Settings::db_table_infected_compo_invites . '` WHERE `id` = ' . $this->id . ';');
+	
+		mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_compo_memberof . '` (`userId`, `clanId`) VALUES (' . $this->userId . ', ' . $this->clanId . ');');
+
+		MySQL::close($con);
 	}
 }
 ?>
