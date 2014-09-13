f6f35d4ddfbe5b479e936e790514e218c7cd5186
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-09-14 00:31:13 +0200

    Added some clan handlers

--- objects/clan.php
+++ objects/clan.php
@@ -1,4 +1,7 @@
 <?php
+require_once 'settings.php';
+require_once 'mysql.php';
+require_once 'handlers/userhandler.php';
 class Clan {
 	private $id;
 	private $chief;
@@ -26,6 +29,21 @@ class Clan {
 
 	public function getEvent() {
 		return $this->event;
+	}
+
+	public function getMembers() {
+		$con = MySQL::open(Settings::db_name_infected_compo);
+
+		$result = mysqli_query($con, 'SELECT * FROM `' . Settings::db_table_infected_compo_memberof . '` WHERE `clanId` = ' . $this->id . ';');
+	
+		$userList = array();
+
+		while($row = mysqli_fetch_array($result)) {
+			array_push($userList, UserHandler::getUser($row['userId']) );
+		}
+
+		MySQL::close($con);
+		return $userList;
 	}
 }
 ?>
