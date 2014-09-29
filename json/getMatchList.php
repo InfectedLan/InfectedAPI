5d55c92ec4049cfea939e1c2e476b51daeb0ba92
Liam svanåsbakken Crouch <petterroea@skymiastudios.com>
2014-09-29 23:38:46 +0200

    JSON function that returns list of current matches

---
+++ json/getMatchList.php
@@ -0,0 +1,51 @@
+<?php
+require_once 'session.php';
+require_once 'handlers/userhandler.php';
+require_once 'handlers/matchhandler.php';
+require_once 'handlers/matchhandler.php';
+
+$result = false;
+$message = null;
+$matchArray = array();
+
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+
+	if($user->hasPermission("*") || $user->hasPermission("functions.compoadmin")) {
+		if(isset($_GET['id'])) {
+			$compo = CompoHandler::getCompo($_GET['id']);
+
+			//First, get pending matches
+			$pendingMatches = MatchHandler::getPendingMatches($compo);
+
+			$pendingArray = array();
+			foreach($pendingMatches as $match) {
+				$matchData = array();
+
+				$matchData['id'] = $match->getId();
+				$matchData['startTime'] = $match->getScheduledTime();
+				$matchData['startString'] = date('d F H:i', $match->getScheduledTime());
+
+				$matchData['participants'] = MatchHandler::getParticipantString($match);
+
+				array_push($pendingArray, $matchData);
+			}
+			$matchArray['pending'] = $pendingArray;
+
+			$result = true;
+		} else {
+			$message = 'Vi mangler felt';
+		}
+	} else {
+		$message = "Du har ikke tillatelse!";
+	}
+} else {
+	$message = 'Du er ikke logget inn.';
+}
+
+if($result) {
+	echo json_encode(array('result' => $result, 'data' => $matchArray));
+} else {
+	echo json_encode(array('result' => $result, 'message' => $message));
+}
+?>
