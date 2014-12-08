9b097476d8d87a83bc61cae79d4d43d4e7587b30
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-12-08 23:09:44 +0100

    Using datetime in database instead of timestamp. Closes issue #52

--- handlers/paymentloghandler.php
+++ handlers/paymentloghandler.php
@@ -2,22 +2,18 @@
 require_once 'settings.php';
 require_once 'mysql.php';
 
-class PaymentLogHandler
-{
-	//Couldnt be bothered to write object for this yet, as we only log for security purposes
-	public static function logPayment($user, $ticketType, $amount, $total, $transactionId)
-	{
-		$time = time();
-
+class PaymentLogHandler {
+	// Could not be bothered to write object for this yet, as we only log for security purposes.
+	public static function logPayment($user, $ticketType, $price, $totalPrice, $transactionId) {
 		$con = MySQL::open(Settings::db_name_infected_tickets);
 
-		mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_tickets_paymentlog . '` (`timeLogged`, `userId`, `ticketType`, `amount`, `totalPrice`, `transactionId`) 
-							VALUES (' . $con->real_escape_string($time) . ', '
-									 . $con->real_escape_string($user->getId()) . ', '
-									 . $con->real_escape_string($ticketType->getId()) . ','
-									 . $con->real_escape_string($amount) . ', '
-									 . $con->real_escape_string($total) . ', 
-									\'' . $con->real_escape_string($transactionId) . '\');');
+		mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_tickets_paymentlog . '` (`userId`, `ticketType`, `price`, `totalPrice`, `transactionId`, `datetime`) 
+								VALUES (\'' . $con->real_escape_string($user->getId()) . '\', 
+										\'' . $con->real_escape_string($ticketType->getId()) . '\', 
+										\'' . $con->real_escape_string($price) . '\', 
+										\'' . $con->real_escape_string($totalPrice) . '\', 
+										\'' . $con->real_escape_string($transactionId) . '\', 
+										\'' . date('Y-m-d H:i:s') . '\');');
 
 		MySQL::close($con);
 	}
