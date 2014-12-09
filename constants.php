cea02b20e308af6cfd71e7126f7b4b3c994af921
Halvor Lyche Strandvoll <halvors@halvors.org>
2014-12-09 13:02:34 +0100

    Moved permission mappings to new constants class.

---
+++ constants.php
@@ -0,0 +1,23 @@
+<?php
+class Constants {
+	public static $permissionList = array(
+			'*' => 'Gir tilgang til absolutt alt, du er nå en udødelig administrator.',
+			'functions.search-users'    => 'Søk etter brukere i databasen.',
+			'functions.my-crew' 	    => 'Endre sider under "My Crew".',
+			'functions.info'			=> 'Post informasjon på infoskjermen som vil vises under arrangementet.',
+			'functions.site-list-games' => 'Administrer spill, settes opp compo i nye spill og moderer påmeldinger.',
+			'functions.site-pages' 	    => 'Endre innholdet på nettsiden.',
+			'functions.seatmap'		    => 'Se seatmappet',
+			'chief.home' 			    => 'Endre "home" siden."',
+			'chief.tickets' 			=> 'Gjøre ting relatert til tickets-siden, slik som flytting av brukere',
+			'chief.groups' 			    => 'Administrer crewene, medlemmene i dem og hvem som er ledere.',
+			'chief.teams' 			    => 'Administrer lagene for ditt crew, hvem som er medlem av hvilke lag og lederene i lagene.',
+			'chief.avatars'			    => 'Administrer profilbilder, alle nye profilbilder vil dukke opp her og du kan godjkenne dem eller avise.',
+			'chief.applications'		=> 'Godta eller avvis crew-søknader.',
+			'admin.events' 			    => 'Administrer arrangementer, informasjon er blir automatisk endret på hovedsiden og ticketsiden.',
+			'admin.permissions' 		=> 'Velg hvilke bruker som skal ha tilgang til hva, dette er et tilgangsystem, men husk at brukere har standard tilganger utenom dette, ettersom de er medlem av crewet eller ikke.',
+			'admin.seatmap' 			=> 'Endre seatmappet, her kan du flytte, legg til, og fjerne seter og rader.',
+			'developer.change-user'	    => 'Lar deg logge inn som hvilken som helst annen bruker, kun beregnet for bruk ved feilsøking.',
+			'developer.console' 		=> 'Gir deg tilgang til vedlikeholdskonsollen brukt av utviklere med over gjennomsnittlig forståelse for systemet.');
+}
+?>
