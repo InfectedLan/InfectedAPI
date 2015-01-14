4ed83ec425cc3e5437721bad63d1f6ea55fd43b2
Liam Svanåsbakken Crouch <petterroea@skymiastudios.com>
2015-01-14 20:21:13 +0100

    Fixed bug in user searching

--- json/user/findUser.php
+++ json/user/findUser.php
@@ -7,23 +7,20 @@ $users = array();
 
 if (Session::isAuthenticated()) {
 	$user = Session::getCurrentUser();
-
-	if ($user->hasPermission('*') ||
-		$user->hasPermission('functions.search-users')) {
-		if (isset($_GET['query']) &&
-			strlen($_GET['query']) >= 2) {
-			$query = preg_replace('/[^A-Za-z0-9]/', ' ', $_GET['query']);
-			$userList = UserHandler::search($query);
-			
-			if (!empty($userList)) {
-				$result = true;
-			
-				foreach ($userList as $userValue) {
-					array_push($users, array('id' => $userValue->getId(),
-											 'firstname' => $userValue->getFirstname(),
-											 'lastname' => $userValue->getLastname(),
-											 'nickname' => $userValue->getNickname()));
-				}
+	
+	if (isset($_GET['query']) &&
+		strlen($_GET['query']) >= 2) {
+		$query = preg_replace('/[^A-Za-z0-9]/', ' ', $_GET['query']);
+		$userList = UserHandler::search($query);
+		
+		if (!empty($userList)) {
+			$result = true;
+		
+			foreach ($userList as $userValue) {
+				array_push($users, array('id' => $userValue->getId(),
+										 'firstname' => $userValue->getFirstname(),
+										 'lastname' => $userValue->getLastname(),
+										 'nickname' => $userValue->getNickname()));
 			}
 		}
 	}
