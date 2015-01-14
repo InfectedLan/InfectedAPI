7847f3d5081d9f9a66b4287ceb56f97a99eadd3c
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-14 21:49:03 +0100

    Fixed ticket transfer duplicate bug.

--- handlers/tickettransferhandler.php
+++ handlers/tickettransferhandler.php
@@ -35,20 +35,20 @@ class TicketTransferHandler {
         $wantedTimeLimit = time() - Settings::ticketTransferTime;
 
         $result = $mysql->query('SELECT * FROM `' . Settings::db_table_infected_tickets_tickettransfers . '` 
-								 WHERE `fromId` = \'' . $mysql->real_escape_string($user->getId()) . '\'
-								 AND `revertable` = \'1\'
-								 AND `datetime` > \'' . date('Y-m-d H:i:s', $wantedTimeLimit) . '\';');
+                                 WHERE `fromId` = \'' . $mysql->real_escape_string($user->getId()) . '\'
+                                 AND `revertable` = \'1\'
+                                 AND `datetime` > \'' . date('Y-m-d H:i:s', $wantedTimeLimit) . '\';');
 
         $transferrableList = array();
 
         while($row = $result->fetch_array()) {
-        	$transferrableTicket = new TicketTransfer($row['id'],
-                                      $row['ticketId'], 
-                                      $row['fromId'],
-                                      $row['toId'],
-                                      $row['datetime'],                              
-                                      $row['revertable']);
-									  
+            $transferrableTicket = new TicketTransfer($row['id'],
+                                                      $row['ticketId'], 
+                                                      $row['fromId'],
+                                                      $row['toId'],
+                                                      $row['datetime'],                              
+                                                      $row['revertable']);
+                                      
             array_push($transferrableList, $transferrableTicket);
         }
 
@@ -67,19 +67,23 @@ class TicketTransferHandler {
 
         $mysql->close();
     }
-	
-	public static function freezeTransfer($transfer) {
-		$mysql = MySQL::open(Settings::db_name_infected_tickets);
+    
+    public static function freezeTransfer($transfer) {
+        $mysql = MySQL::open(Settings::db_name_infected_tickets);
 
-		$result = $mysql->query('UPDATE `' . Settings::db_table_infected_tickets_tickettransfers .  '` 
-								 SET `revertable`=\'0\'
-								 WHERE `id` = \'' . $mysql->real_escape_string($transfer->getId()) . '\';');
-	}
+        $result = $mysql->query('UPDATE `' . Settings::db_table_infected_tickets_tickettransfers .  '` 
+                                 SET `revertable` = \'0\'
+                                 WHERE `id` = \'' . $mysql->real_escape_string($transfer->getId()) . '\';');
+								 
+		$mysql->close();
+    }
     
     /*
      * Transfers a given ticket from it's current user to the user specified.
      */
     public static function transfer($ticket, $user) {
+		$transfer = self::getTransferFromTicket($ticket);
+	
         // Check that the ticket is for current event, we don't allow transfers of old tickets.
         if ($ticket->getEvent()->getId() == EventHandler::getCurrentEvent()->getId()) {
         
@@ -88,6 +92,11 @@ class TicketTransferHandler {
             
                 // Check that the new user isn't the same as the old one.
                 if ($ticket->getUser()->getId() != $user->getId()) {
+					// Prevent the original transfer from being reverted.
+                    if ($transfer != null) {
+						self::freezeTransfer($transfer);
+					}
+				
                     // Add row to ticket transfers table.
                     self::createTransfer($ticket, $user, true);
                                                 
@@ -104,7 +113,7 @@ class TicketTransferHandler {
     public static function revertTransfer($ticket, $user) {
         $transfer = self::getTransferFromTicket($ticket);
         $timeLimit = Settings::ticketTransferTime;
-        
+		
         // Check that the ticket is for current event, we don't allow reverting transfers for old tickets.
         if ($ticket->getEvent()->getId() == EventHandler::getCurrentEvent()->getId()) {
         
@@ -112,35 +121,35 @@ class TicketTransferHandler {
             if (!$ticket->isCheckedIn()) {
             
                 // Check if the user specified matches the former user of the ticket.            
-				if ($transfer->getFrom()->getId() == $user->getId()) {
-	
-					// Check if the ticket is revertable.
-					if ($transfer->isRevertable()) {
+                if ($transfer->getFrom()->getId() == $user->getId()) {
+    
+                    // Check if the ticket is revertable.
+                    if ($transfer->isRevertable()) {
 					
-						// Check that the time limit isn't run out.
-						if ($transfer->getDateTime() + $timeLimit >= time()) {
-							// Add row to ticket transfers table.
-							self::createTransfer($ticket, $transfer->getFrom(), false);
-							
-							// Prevent the original transfer from being reverted.
-							self::freezeTransfer($transfer->getId());
-							
-							// Actually change the user of the ticket.
-							TicketHandler::updateTicketUser($ticket, $user);
-						} else {
-							return 'Det er for sent å overføre denne billetten!';
-						}
-					} else {
-						return 'Billeten kan ikke angres.';
-					}
+                        // Check that the time limit isn't run out.
+                        if ($transfer->getDateTime() + $timeLimit >= time()) {
+                            // Prevent the original transfer from being reverted.
+                            self::freezeTransfer($transfer);
+                            
+                            // Add row to ticket transfers table.
+                            self::createTransfer($ticket, $transfer->getFrom(), false);
+                            
+                            // Actually change the user of the ticket.
+                            TicketHandler::updateTicketUser($ticket, $user);
+                        } else {
+                            return 'Det er for sent å overføre denne billetten!';
+                        }
+                    } else {
+                        return 'Billeten kan ikke angres.';
+                    }
                 } else {
-                	return 'Du er ikke den gamle eieren av billetten!';
+                    return 'Du er ikke den gamle eieren av billetten!';
                 }
             } else {
-            	return 'Billetten er sjekket inn!';
+                return 'Billetten er sjekket inn!';
             }
         } else {
-        	return 'Billetten er for et gammelt arrangement!';
+            return 'Billetten er for et gammelt arrangement!';
         }
     }
 }
