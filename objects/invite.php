c6c0f00a4d1d7ab74f860695f43c0ee250b0df18
Liam Svanåsbakken Crouch <petterroea@skymiastudios.com>
2015-01-19 01:14:03 +0100

    Made clan handler automatically set user to stepin if team is full

--- objects/invite.php
+++ objects/invite.php
@@ -2,6 +2,7 @@
 require_once 'mysql.php';
 require_once 'settings.php';
 require_once 'objects/object.php';
+require_once 'handlers/clanhandler.php';
 
 class Invite extends Object{
 	private $userId;
@@ -34,8 +35,17 @@ class Invite extends Object{
 		$con = MySQL::open(Settings::db_name_infected_compo);
 
 		mysqli_query($con, 'DELETE FROM `' . Settings::db_table_infected_compo_invites . '` WHERE `id` = ' . $this->getId() . ';');
-	
-		mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_compo_memberof . '` (`userId`, `clanId`) VALUES (' . $this->userId . ', ' . $this->clanId . ');');
+
+		$clan = ClanHandler::getClan($this->getClanId());
+
+		$memberList = ClanHandler::getPlayingMembers($clan);
+		$compo = ClanHandler::getCompo($clan);
+
+		if(count($memberList) < $compo->getTeamSize()) {
+			mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_compo_memberof . '` (`userId`, `clanId`, `stepin`) VALUES (' . $this->userId . ', ' . $this->clanId . ', 0);');
+		} else {
+			mysqli_query($con, 'INSERT INTO `' . Settings::db_table_infected_compo_memberof . '` (`userId`, `clanId`, `stepin`) VALUES (' . $this->userId . ', ' . $this->clanId . ', 1);');
+		}
 
 		MySQL::close($con);
 	}
