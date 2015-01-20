63408e84536b67267b82312d7ad3225be106affb
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-20 12:30:17 +0100

    Added support for sending emails to all by passing 0 as the id in userList.

--- json/email/sendEmail.php
+++ json/email/sendEmail.php
@@ -10,15 +10,26 @@ if (Session::isAuthenticated()) {
 	
 	if ($user->hasPermission('*') ||
 		$user->hasPermission('admin.email')) {
-		if (isset($_GET['user']) &&
+		if (isset($_GET['userIdList']) &&
 			isset($_GET['subject']) &&
 			isset($_GET['message']) &&
-			is_numeric($_GET['user']) &&
 			!empty($_GET['subject']) &&
 			!empty($_GET['message'])) {
-			$to = UserHandler::getUser($_GET['user']);
+			$userIdList = explode(',', $_GET['userIdList']);
+			$userList = array();
+			
+			// If the list of user contains "0", that means send it to anyone.
+			if (in_array(0, $userIdList)) {
+				$userList = UserHandler::getUsers();
+			} else {
+				// Build the userList from the given id's
+				foreach ($userIdList as $userId) {
+					array_push($userList, UserHandler::getUser($userId));
+				}
+			}
+			
 			// Sends emails to users in userList with the given subject and message.
-			MailManager::sendEmails($to, $_GET['subject'], $_GET['message']);
+			MailManager::sendEmails($userList, $_GET['subject'], $_GET['message']);
 			$message = 'Din e-post ble sendt til de oppgitte mottakeren.';
 			$result = true;
 		} else {
