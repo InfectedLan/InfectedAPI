75100a6515dc9113a0c3835b2edc4f828cc113b5
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-01-27 00:46:12 +0100

    Refactored sendEmail in json API.

--- json/email/sendEmail.php
+++ json/email/sendEmail.php
@@ -10,7 +10,7 @@ if (Session::isAuthenticated()) {
 	$user = Session::getCurrentUser();
 	
 	if ($user->hasPermission('*') ||
-		$user->hasPermission('admin.email')) {
+		$user->hasPermission('chief.email')) {
 		if (isset($_GET['userIdList']) &&
 			isset($_GET['subject']) &&
 			isset($_GET['message']) &&
@@ -25,26 +25,34 @@ if (Session::isAuthenticated()) {
 				$value = $userIdList[0];
 				
 				// Match the given group of users, and build the array.
-				if ($value == 'all') {
-					$userList = UserHandler::getUsers();
-				} else if ($value == 'allMembers') {
-					$userList = UserHandler::getMemberUsers();
-				} else if ($value == 'allNonMembers') {
-					$userList = UserHandler::getNonMemberUsers();
-				} else if ($value == 'allWithTicket') {
-					$userList = UserHandler::getParticipantUsers(EventHandler::getCurrentEvent());
-				} else if ($value == 'allWithTickets') {
-					$participantList = UserHandler::getParticipantUsers(EventHandler::getCurrentEvent());
-					
-					// Check which users have more than 1 ticket.
-					foreach ($participantList as $participant) {
-						// If the participant user have more than 1 ticket, add him/her to the user list.
-						if (count($participant->getTickets()) > 1) {
-							array_push($userList, $participant);
+				if ($user->hasPermission('*')) {
+					if ($value == 'all') {
+						$userList = UserHandler::getUsers();
+					} else if ($value == 'allMembers') {
+						$userList = UserHandler::getMemberUsers();
+					} else if ($value == 'allNonMembers') {
+						$userList = UserHandler::getNonMemberUsers();
+					} else if ($value == 'allWithTicket') {
+						$userList = UserHandler::getParticipantUsers(EventHandler::getCurrentEvent());
+					} else if ($value == 'allWithTickets') {
+						$participantList = UserHandler::getParticipantUsers(EventHandler::getCurrentEvent());
+						
+						// Check which users have more than 1 ticket.
+						foreach ($participantList as $participant) {
+							// If the participant user have more than 1 ticket, add him/her to the user list.
+							if (count($participant->getTickets()) > 1) {
+								array_push($userList, $participant);
+							}
 						}
+					} else if ($value == 'allWithTicketLast3') {
+						$userList = UserHandler::getPreviousParticipantUsers();
 					}
-				} else if ($value == 'allWithTicketLast3') {
-					$userList = UserHandler::getPreviousParticipantUsers();
+				}
+				
+				if ($user->isGroupMember()) {
+					if ($value == 'group') {
+						$userList = $user->getGroup()->getMembers();
+					}
 				}
 			} else {
 				// Build the userList from the given id's
--- json/email/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'session.php';
-require_once 'mailmanager.php';
-require_once 'handlers/userhandler.php';
-
-$userList = UserHandler::getPreviousParticipantUsers();
-
-var_dump($userList);
-?>
