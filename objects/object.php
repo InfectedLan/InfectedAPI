009c237a8675db093690b90d4d4fd7a61ed89877
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-02-11 12:52:11 +0100

    Added compare() function to Object.
    This can be used to compare all classes that extends Object against each
    other.
    
    Should be used instead of just comparing the internal id against each
    other.

--- objects/object.php
+++ objects/object.php
@@ -8,5 +8,21 @@ class Object {
 	public function getId() {
 		return $this->id;
 	}
+
+	/*
+	 * Compare is a function that allows you to easily compare this object with the specified one. 
+	 * It returns true if the specified object is identical to this one.
+	 */
+	public function compare($object) {
+		// Check that the specified object is an instance of this one.
+		if ($object instanceof $this) {
+			// Compare this objects by the internal id.
+			if ($object == $this) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
 ?>
