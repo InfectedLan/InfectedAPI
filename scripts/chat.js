287d02f791286dfcd96251c35a38b21bff1d150d
Liam Svanåsbakken Crouch <petterroea@skymiastudios.com>
2015-02-11 19:49:12 +0100

    Work on styling

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,12 +8,16 @@ $(document).ready(function() {
 function createChat(divId, chatId, height) {
 	chatList.push({"divId": divId, "chatId": chatId, "lastId": -1});
 	console.log("Created chat id " + chatId + " for divid " + divId + " with height " + height);
-	$("#" + divId).html('<div class="chatArea" height="' + (height-40) + '"></div><input type="text" class="chatBox" />');
+	$("#" + divId).html('<div class="chatArea" style="height: ' + (height-25-5-10) + 'px;"></div><div style="padding-right:5px;padding-left:0px;margin-right:2px;"><input type="text" placeholder="Skriv her, trykk enter for å sende" class="chatBox" /></div>');
 	//Listen to enter key
 	$("#" + divId).find('.chatBox').keypress({chat: chatId, div: divId}, function(e) {
 	    if(e.which == 13) {
-	        sendChat(e.data.chat, $(this).val());
-	        $(this).val("");
+	    	if($(this).val().length > 0) {
+	    		sendChat(e.data.chat, $(this).val());
+	        	$(this).val("");
+	    	} else {
+	    		error("Chatmeldingen er for kort!");
+	    	}
 	    }
 	});
 }
@@ -37,34 +41,41 @@ function updateChats() {
 			i--;
 		} else {
 			//Download new data
-			$.getJSON('../api/json/chat/getLastChatMessage.php?id=' + chatList[i].chatId, function(data){ 
-				if(data.result != false) { //result is an object if successfull
-					if(data.result.id != chatList[i]) {
-						//Download chat data
-						$.getJSON('../api/json/chat/getLastChatMessages.php?id=' + chatList[i].chatId + '&count=500', function(chatData){ 
-							if(chatData.result != false) {
-								for(var x = chatData.result.length-1; x >= 0; x--) {
-									if(chatData.result[x].admin) {
-										$("#" + chatList[i].divId).find(".chatArea").append("<p><b>[Admin]" + chatData.result[x].user + "</b>: " + chatData.result[x].message + "</p>");
+			$.getJSON('../api/json/chat/getLastChatMessage.php?id=' + chatList[i].chatId, (function() {
+				var chatListIndex = i;
+				return function(data){ 
+					if(data.result != false) { //result is an object if successfull
+						if(data.result.id != chatList[chatListIndex].lastId) {
+							//Download chat data
+							$.getJSON('../api/json/chat/getLastChatMessages.php?id=' + chatList[chatListIndex].chatId + '&count=500', (function () {
+
+								return function(chatData){ 
+									if(chatData.result != false) {
+										$("#" + chatList[chatListIndex].divId).find(".chatArea").html("");
+										for(var x = chatData.result.length-1; x >= 0; x--) {
+											if(chatData.result[x].admin) {
+												$("#" + chatList[chatListIndex].divId).find(".chatArea").append("<span><b>[Admin]" + chatData.result[x].user + "</b>: " + chatData.result[x].message + "<br /></span>");
+											} else {
+												$("#" + chatList[chatListIndex].divId).find(".chatArea").append("<span>" + chatData.result[x].user + ": " + chatData.result[x].message + "<br /></span>");
+											}
+										}
+										//Tell the array that we have the newest content
+										chatList[chatListIndex].lastId = data.result.id;
 									} else {
-										$("#" + chatList[i].divId).find(".chatArea").append("<p>" + chatData.result[x].user + ": " + chatData.result[x].message + "</p>");
+										console.log("Something went wrong during fetching the chat data: " + chatData.message);
+										error("Det skjedde en feil under hentingen av chat-data: <br />" + chatData.message);
 									}
-								}
-								//Tell the array that we have the newest content
-								chatList[i].lastId = data.result.id;
-							} else {
-								console.log("Something went wrong during fetching the chat data: " + chatData.message);
-								error("Det skjedde en feil under hentingen av chat-data: <br />" + chatData.message);
-							}
-						});
+								};
+							}) ());
+						} else {
+							//All ok, chat is updated
+						}
 					} else {
-						//All ok, chat is updated
+						console.log("Something went wrong during fetching the chat: " + data.message);
+						error(data.message);
 					}
-				} else {
-					console.log("Something went wrong during fetching the chat: " + data.message);
-					error(data.message);
-				}
-			});
+				};
+			}) () );
 		}
 	}
 }
