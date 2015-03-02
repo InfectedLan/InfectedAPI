58112ae0a2e4cc250a14da4a781359c232f9eeb1
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-03-02 19:46:24 +0100

    Added GPL header.

--- scripts/seatmapRenderer.js
+++ scripts/seatmapRenderer.js
@@ -1,18 +1,35 @@
+/*
+ * This file is part of InfectedAPI.
+ *
+ * Copyright (C) 2015 Infected <http://infected.no/>.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var seatmapData = null;
-function downloadAndRenderSeatmap(target, seatHandlerFunction, callback)
-{
+
+function downloadAndRenderSeatmap(target, seatHandlerFunction, callback) {
 	$.getJSON('../api/json/seatmap/seatmapAvailability.php?id=' + seatmapId, function(data){
-		if(data.result)
-		{
+		if(data.result) {
 			seatmapData = data;
 			renderSeatmap(target, seatHandlerFunction, callback);
-		}
-		else
-		{
+		} else {
 			$("#seatmapCanvas").html('<i>En feil oppstod under håndteringen av seatmappet...</i>');
 		}
   	});
 }
+
 //Target is required, seatHandlerFunction isnt
 function renderSeatmap(target, seatHandlerFunction, callback) {
 	//Render seatmap
@@ -32,6 +49,7 @@ function renderSeatmap(target, seatHandlerFunction, callback) {
 			}
 			//Run the seat handler function if set
 			var customClass = seatmapData.rows[i].seats[s].occupied ? "taken" : "free";
+			
 			if(typeof seatHandlerFunction !== "undefined")
 			{
 				var customClassCheck = seatHandlerFunction( seatmapData.rows[i].seats[s].id, 
