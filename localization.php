0de1301a3de68ab9b393e2882682f4893cca9b11
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-04-07 16:05:38 +0200

    Added support for locales with an argument.

--- localization.php
+++ localization.php
@@ -33,28 +33,39 @@ class Localization {
 		// Otherwise, return an error string.
 		return 'Locale not found, this is a bug.';
 	}
+
+	/*
+	 * Get locale by key, with the given replace argument.
+	 */
+	public static function getLocaleWithArgument($key, $argument) {
+		return str_replace('%s', $argument, self::getLocale($key));
+	}
 }
 
 // Initialize the list.
-Localization::$localizationList = array('you_are_not_logged_in' 								=> 'Du er ikke logget inn.',
-										'you_do_not_have_permission_to_do_that' 				=> 'Du har ikke rettigheter til dette.',
-										'you_have_not_filled_out_the_required_fields'			=> 'Du har ikke fylt ut de nødvendige feltene.',
-										'the_object_you_are_trying_to_change_does_not_exist' 	=> 'Objektet du prøver å endre, finnes ikke.',
-										'the_object_you_are_trying_to_remove_does_not_exist' 	=> 'Objektet du prøver å fjerne, finnes ikke.',
-										'no_object_specified' 									=> 'Ikke noe objekt spesifisert.',
+Localization::$localizationList = array('you_are_not_logged_in' 							 => 'Du er ikke logget inn.',
+										'you_do_not_have_permission_to_do_that' 			 => 'Du har ikke rettigheter til dette.',
+										'you_have_not_filled_out_the_required_fields'		 => 'Du har ikke fylt ut de nødvendige feltene.',
+										'no_object_specified' 								 => 'Ikke noe objekt spesifisert.',
+										'you_are_already_in_a_group'						 => 'Du er allerede i et crew.';
 										
 										/* JSON */
-										// Application
-										'you_can_not_approve_applications_from_previous_events' => 'Du kan ikke godkjenne søknader fra tidligere arrangementer.',
-										'this_application_does_not_exist' 						=> 'Denne søknaden finnes ikke.',
+										// Agenda
+										'the_agenda_you_are_trying_to_change_does_not_exist' => 'Agenda\'en du prøver å endre, finnes ikke.',
+										'the_agenda_you_are_trying_to_remove_does_not_exist' => 'Agenda\'en du prøver å fjerne, finnes ikke.',
 
-										'no_application_specified' 								=> 'Ingen søknad er spesifisert.',
-										'' => '',
-										'' => '',
-										'' => '',
-										'' => '',
-										'' => '',
-										'' => '',
-										'' => '',
+										// Application
+										'this_application_does_not_exist'	=> 'Denne søknaden finnes ikke.',
+										'no_application_specified'			=> 'Ingen søknad er spesifisert.',
+										
+										'you_have_already_submitted_an_application_to_value_you_can_apply_again_if_your_application_should_be_denied' 	=> 'Du har allerede søkt til "%s". Du kan søke igjen hvis søknaden din skulle bli avslått.',
+										'you_must_upload_a_valid_picture_before_you_can_submit_an_application' 											=> 'Du må laste opp en gyldig bilde før du kan levere en søknad.',
+										'your_appliction_to_value_is_now_submitted' 																	=> 'Din søknad til crewet "%s" er nå sendt.',
+										
+										'you_can_not_approve_applications_from_previous_events' 			=> 'Du kan ikke godkjenne søknader fra tidligere arrangementer.',
+										'you_can_not_reject_applications_from_previous_events' 				=> 'Du kan ikke avslå søknader fra tidligere arrangementer.',
+										'you_must_provide_a_reason_why_the_application_shall_be_rejected' 	=> 'Du må oppgi en grunn på hvorfor søkneden skal bli avvist.',
+										'you_can_not_queue_applications_from_previous_events' 				=> 'Du kan ikke putte søknader fra tidligere arrangementer i køen.',
+										'you_can_not_unqueue_applications_from_previous_events'				=> 'Du kan ikke ta søknader fra tidligere arrangementer ut av køen.',
 										'' => '');
 ?>
