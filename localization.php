2186129a7b9510a7c27d384d628d19d99146b1a6
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-04-28 09:44:40 +0200

    Implemented auto detection of language.

--- localization.php
+++ localization.php
@@ -19,19 +19,30 @@
  */
 
 class Localization {
-	static $list;
+	static $defaultLanguage = 'nb_NO';
 
 	/*
 	 * Get locale by key.
 	 */
-	public static function getLocale($key, $args...) {
+	public static function getLocale($key) {
+		$lang = Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+		$path = 'http://crew.test.infected.no/api/resources/lang/';
+		$filename = $path . $lang . '.json';
+
+		// Check if language exists, if not fallback to default language.
+		if (!file_exists($filename)) {
+		    $filename = $path . self::$defaultLanguage . '.json';
+		}
+
+		$list = json_decode(file_get_contents($filename), true);
+
 		// If key exists in array, return the value.
-		if (array_key_exists($key, self::$list)) {
-			return self::$list[$key];
+		if (array_key_exists($key, $list)) {
+			return $list[$key];
 		}
 
 		// Otherwise, return an error string.
-		return 'Locale not found, this is a bug so please submit a report at https://github.com/InfectedLan/InfectedAPI/issues';
+		return 'Locale not found in language \'' . $lang . '\', this is a bug. Please submit a report at https://github.com/InfectedLan/InfectedAPI/issues';
 	}
 
 	/*
@@ -41,7 +52,4 @@ class Localization {
 		return sprintf(self::getLocale($key), $argument);
 	}
 }
-
-// Initialize the list.
-Localization::$list = json_decode(file_get_contents('http://crew.test.infected.no/api/resources/lang/nb_NO.json'), true);
 ?>
