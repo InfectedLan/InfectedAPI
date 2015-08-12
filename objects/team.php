721418e8fa6f377d765d05b1015d3f1265526db8
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-08-12 12:35:13 +0200

    Fixed a bug caused by a silly typo.

--- objects/team.php
+++ objects/team.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -31,47 +31,47 @@ class Team extends EventObject {
 	private $title;
 	private $description;
 	private $leaderId;
-	
+
 	/*
 	 * Returns the group for this team.
 	 */
 	public function getGroup() {
 		return GroupHandler::getGroupByEvent($this->getEvent(), $this->groupId);
 	}
-	
+
 	/*
 	 * Returns the name of this team.
 	 */
 	public function getName() {
 		return $this->name;
 	}
-	
+
 	/*
 	 * Returns the title of this team.
 	 */
 	public function getTitle() {
 		return $this->title;
 	}
-	
+
 	/*
 	 * Return the description for this team.
 	 */
 	public function getDescription() {
 		return $this->description;
 	}
-	
+
 	/*
 	 * Returns the leader of this team.
 	 */
 	public function getLeader() {
 		return UserHandler::getUser($this->leaderId);
 	}
-	
-	/* 
+
+	/*
 	 * Returns an array of users that are members of this group.
 	 */
 	public function getMembers() {
-		return TeamHandler::getMembersBvent($this->getEvent(), $this);
+		return TeamHandler::getMembersByEvent($this->getEvent(), $this);
 	}
 }
 ?>
