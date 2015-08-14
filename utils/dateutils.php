466b08949125554ad5502fd7d51b9309b79d08d4
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-08-14 12:23:51 +0200

    Cleanup.

--- utils/dateutils.php
+++ utils/dateutils.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -21,30 +21,30 @@
 class DateUtils {
 	public static function getDayFromInt($day) {
 		$dayList = array('Søndag',
-						 'Mandag', 
-						 'Tirsdag', 
-						 'Onsdag', 
-						 'Torsdag', 
-						 'Fredag', 
-						 'Lørdag');
-		
+						 				 'Mandag',
+						 			 	 'Tirsdag',
+						 				 'Onsdag',
+						 				 'Torsdag',
+						 				 'Fredag',
+						 				 'Lørdag');
+
 		return $dayList[$day];
 	}
-	
+
 	public static function getMonthFromInt($month) {
-		$monthList = array('Januar', 
-						   'Februar', 
-						   'Mars', 
-						   'April', 
-						   'Mai', 
-						   'Juni', 
-						   'Juli', 
-						   'August', 
-						   'September', 
-						   'Oktober', 
-						   'November', 
-						   'Desember');
-		
+		$monthList = array('Januar',
+						   				 'Februar',
+										   'Mars',
+										   'April',
+										   'Mai',
+										   'Juni',
+										   'Juli',
+										   'August',
+										   'September',
+										   'Oktober',
+										   'November',
+										   'Desember');
+
 		return $monthList[$month - 1];
 	}
 }
--- utils/stringutils.php
+++ utils/stringutils.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -24,7 +24,7 @@ class StringUtils {
 	}
 
 	public static function endsWith($haystack, $needle) {
-   		return (strpos(strrev($haystack), strrev($needle)) === 0);
+  	return (strpos(strrev($haystack), strrev($needle)) === 0);
 	}
 }
 ?>
