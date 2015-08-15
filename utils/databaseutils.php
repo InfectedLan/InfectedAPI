679526cc9871459c96ef82562e62b429aeb0b928
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-08-15 23:40:58 +0200

    Fixed an invalid SQL query in DatabaseUtils.

--- utils/databaseutils.php
+++ utils/databaseutils.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -28,10 +28,10 @@ class DatabaseUtils {
 	 */
 	public static function copyTable($databaseName, $fromTableName, $toTableName) {
 		$database = Database::open($databaseName);
-		
+
 		$database->query('INSERT INTO `' . $toTableName . '`
-						  SELECT * FROM `' . $fromTableName . '`;');
-		
+						  				SELECT * FROM `' . $fromTableName . '`;');
+
 		$database->close();
 	}
 
@@ -40,23 +40,23 @@ class DatabaseUtils {
 	 */
 	public static function copyTableSelection($databaseName, $tableName, $referenceField, $fromValue, $toValue) {
 		$database = Database::open($databaseName);
-		
-		$database->query('CREATE TEMPORARY TABLE `temporary` 
-								  SELECT * FROM `' . $tableName . '`
-								  WHERE `' . $referenceField . '` = \'' . $fromValue . '\';');
-		
+
+		$database->query('CREATE TEMPORARY TABLE `temporary`
+								  		SELECT * FROM `' . $tableName . '`
+								  		WHERE `' . $referenceField . '` = \'' . $fromValue . '\';');
+
 		$database->query('UPDATE `temporary` SET `id` = NULL;');
-		
-		$database->query('UPDATE `temporary` SET `' . $referenceField . '` = \'' . $toValue . '\' 
-								  WHERE `' . $referenceField . '` = \'' . $fromValue . '\';');
-		
-			$database->query('INSERT INTO `' . $tableName . '`
-									  SELECT * FROM `temporary`
-						  AND NOT EXISTS (SELECT `id` FROM `' . $tableName . '`
-										  WHERE `' . $referenceField . '` = \'' . $toValue . '\');');
-		
-			$database->query('DROP TABLE `temporary`;');
-		
+
+		$database->query('UPDATE `temporary` SET `' . $referenceField . '` = \'' . $toValue . '\'
+								  		WHERE `' . $referenceField . '` = \'' . $fromValue . '\';');
+
+		$database->query('INSERT INTO `' . $tableName . '`
+									  	SELECT * FROM `temporary`
+						  				WHERE NOT EXISTS (SELECT `id` FROM `' . $tableName . '`
+										  									WHERE `' . $referenceField . '` = \'' . $toValue . '\');');
+
+		$database->query('DROP TABLE `temporary`;');
+
 		$database->close();
 	}
 }
