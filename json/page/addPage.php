81dbc12bf20a893699fb8fd5fbbcbbe918c416b0
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-08-16 21:03:12 +0200

    Code cleanup.

--- json/page/addPage.php
+++ json/page/addPage.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,7 +27,7 @@ $message = null;
 
 if (Session::isAuthenticated()) {
 	$user = Session::getCurrentUser();
-	
+
 	if ($user->hasPermission('*') ||
 		$user->hasPermission('admin.website')) {
 		if (isset($_GET['title']) &&
@@ -39,6 +39,7 @@ if (Session::isAuthenticated()) {
 			$content = $_GET['content'];
 			
 			PageHandler::createPage($name, $title, $content);
+
 			$result = true;
 		} else {
 			$message = Localization::getLocale('you_have_not_filled_out_the_required_fields');
--- json/page/editPage.php
+++ json/page/editPage.php
@@ -8,12 +8,12 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3.0 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,7 +27,7 @@ $message = null;
 
 if (Session::isAuthenticated()) {
 	$user = Session::getCurrentUser();
-	
+
 	if ($user->hasPermission('*') ||
 		$user->hasPermission('admin.website')) {
 		if (isset($_GET['id']) &&
@@ -39,9 +39,10 @@ if (Session::isAuthenticated()) {
 			$page = PageHandler::getPage($_GET['id']);
 			$title = $_GET['title'];
 			$content = $_GET['content'];
-			
+
 			if ($page != null) {
 				PageHandler::updatePage($page, $title, $content);
+				
 				$result = true;
 			} else {
 				$message = Localization::getLocale('this_page_does_not_exist');
