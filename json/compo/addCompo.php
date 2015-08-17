d1763cbdc180473f9b428efed9236b7406699056
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-08-17 15:31:59 +0200

    Added addCompo.php to json api.

---
+++ json/compo/addCompo.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * This file is part of InfectedAPI.
+ *
+ * Copyright (C) 2015 Infected <http://infected.no/>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'session.php';
+require_once 'localization.php';
+require_once 'handlers/compohandler.php';
+require_once 'handlers/eventhandler.php';
+
+$result = false;
+$message = null;
+
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+
+	if ($user->hasPermission('*') ||
+		$user->hasPermission('event.agenda')) {
+		if (isset($_GET['title']) &&
+			isset($_GET['tag']) &&
+			isset($_GET['startTime']) &&
+			isset($_GET['startDate']) &&
+			isset($_GET['registrationEndTime']) &&
+			isset($_GET['registrationEndDate']) &&
+			isset($_GET['teamSize']) &&
+			!empty($_GET['title']) &&
+			!empty($_GET['tag']) &&
+			!empty($_GET['startTime']) &&
+			!empty($_GET['startDate']) &&
+			!empty($_GET['registrationEndTime']) &&
+			!empty($_GET['registrationEndDate']) &&
+			is_numeric($_GET['teamSize'])) {
+
+			$name = strtolower(str_replace(' ', '-', $_GET['title']));
+			$title = $_GET['title'];
+			$tag = $_GET['tag'];
+			$description = $_GET['description'];
+			$mode = $_GET['mode'];
+			$price = is_numeric($_GET['price']) ? $_GET['price'] : 0;
+			$startTime = $_GET['startDate'] . ' ' . $_GET['startTime'];
+			$startTime = $_GET['registrationEndDate'] . ' ' . $_GET['registrationEndTime'];
+			$teamSize = $_GET['teamSize'];
+
+			CompoHandler::createCompo($name, $title, $tag, $description, $mode, $price, $startTime, $registrationEndTime, $teamSize);
+
+			$result = true;
+		} else {
+			$message = Localization::getLocale('you_have_not_filled_out_the_required_fields');
+		}
+	} else {
+		$message = Localization::getLocale('you_do_not_have_permission_to_do_that');
+	}
+} else {
+	$message = Localization::getLocale('you_are_not_logged_in');
+}
+
+header('Content-Type: text/plain');
+echo json_encode(array('result' => $result, 'message' => $message), JSON_PRETTY_PRINT);
+?>
