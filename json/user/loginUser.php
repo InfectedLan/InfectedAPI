b8b183c57e2202bc296b2766aa01d88083303292
Halvor Lyche Strandvoll <halvors@halvors.org>
2015-09-23 15:28:41 +0200

    Using POST method insted of GET method for user logins.

--- json/user/loginUser.php
+++ json/user/loginUser.php
@@ -26,12 +26,12 @@ $result = false;
 $message = null;
 
 if (!Session::isAuthenticated()) {
-	if (isset($_GET['identifier']) &&
-		isset($_GET['password']) &&
-		!empty($_GET['identifier']) &&
-		!empty($_GET['password'])) {
-		$identifier = $_GET['identifier'];
-		$password = hash('sha256', $_GET['password']);
+	if (isset($_POST['identifier']) &&
+		isset($_POST['password']) &&
+		!empty($_POST['identifier']) &&
+		!empty($_POST['password'])) {
+		$identifier = $_POST['identifier'];
+		$password = hash('sha256', $_POST['password']);
 
 		if (UserHandler::hasUser($identifier)) {
 			$user = UserHandler::getUserByIdentifier($identifier);
