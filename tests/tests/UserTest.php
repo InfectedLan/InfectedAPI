4458332ea3e5dd5273a913fb5ac616e1b43223ee
Liam Svanåsbakken Crouch <spam@petterroea.com>
2016-07-27 00:15:25 +0200

    removed 5.5.9 php and started porting user test

--- tests/tests/UserTest.php
+++ tests/tests/UserTest.php
@@ -1,26 +1,17 @@
 <?php
-require_once 'testApi/TestSuite.php';
-require_once 'testApi/TestResult.php';
+use PHPUnit\Framework\TestCase;
 
 require_once 'handlers/userhandler.php';
 require_once 'objects/user.php';
 
 /*
- * HEY!
- * 
- * I know this is overly commented, but that is the meaning. This is a "tutorial" on how we assert stuff, and so i explain a lot of stuff
- * If you are learning test writing from this, try to learn the way of thinking from reading the code :)
- *
- * - Liam
- */
-/*
- * UserTestSuite
+ * UserTest
  *
  * Responsible for testing UserHandler and the User object
  *
+ * First class converted to phpunit, lol
  */
-class UserTestSuite extends TestSuite {
-	//Override this
+class UserTest extends TestCase {
 	public function test() {
 		$this->userCreationTest();
 	}
@@ -28,14 +19,14 @@ class UserTestSuite extends TestSuite {
 	private function userCreationTest() {
 		//We expect 21 users to exist from the deployment code
 		$users = UserHandler::getUsers(); //Get users
-		$this->assert_greater_than(count($users), 0); //This asserts if the number of users is 21. It will fail if the left side does not equal the right side
+		$this->assertGreaterThan(count($users), 0); //This asserts if the number of users is 21. It will fail if the left side does not equal the right side
 
 		//Check that the user does not exist. This is done to test that the function does not return bogus data, and is a reccomended thing to test. Test everything, basically.
 		$user = UserHandler::getUserByIdentifier("assertUser");
-		$this->assert_equals($user, null);
+		$this->assertEquals($user, null);
 		//Check that we can get the user by email
 		$user = UserHandler::getUserByIdentifier("assertUser@infected.no");
-		$this->assert_equals($user, null);
+		$this->assertEquals($user, null);
 
 		//Let's create another user
 		$createdUser = UserHandler::createUser("assertionFirstname", 
@@ -50,32 +41,32 @@ class UserTestSuite extends TestSuite {
 											   1337, 
 											   "AssertNick");
 
-		if ($this->assert_not_equals($createdUser, null)) {
+		if ($this->assertNotEquals($createdUser, null)) {
 			//Check that we can get the user by username
 			$user = UserHandler::getUserByIdentifier("assertUser");
-			$this->assert_not_equals($user, null);
+			$this->assertNotEquals($user, null);
 			//Check that we can get the user by email
 			$email_user = UserHandler::getUserByIdentifier("assertUser@infected.no");
-			$this->assert_not_equals($email_user, null);
+			$this->assertNotEquals($email_user, null);
 
 			//Check if the two accounts are the same account
-			$this->assert_equals($user->getId(), $email_user->getId());
+			$this->assertEquals($user->getId(), $email_user->getId());
 			//Check if this is the user we inserted
-			$this->assert_equals($user->getId(), $createdUser->getId());
+			$this->assertEquals($user->getId(), $createdUser->getId());
 
 			//Check that the fields we inserted are intact
-			$this->assert_equals($user->getFirstname(), "assertionFirstname");
-			$this->assert_equals($user->getLastname(), "assertionLastname");
-			$this->assert_equals($user->getUsername(), "assertUser");
-			$this->assert_equals($user->getPassword(), "32cdb619196200050ab0af581a10fb83cfc63b1a20f58d4bafb6313d55a3f0e9");
-			$this->assert_equals($user->getEmail(), "assertUser@infected.no");
-			$this->assert_equals($user->getBirthdate(), 890953200);
-			$this->assert_equals($user->getGenderAsString(), "Gutt");
-			$this->assert_equals($user->getPhoneAsString(), "12 34 56 78");
-			$this->assert_equals($user->getPhone(), 12345678);
-			$this->assert_equals($user->getAddress(), "Test address");
-			$this->assert_equals($user->getPostalCode(), "1337");
-			$this->assert_equals($user->getNickname(), "AssertNick");
+			$this->assertEquals($user->getFirstname(), "assertionFirstname");
+			$this->assertEquals($user->getLastname(), "assertionLastname");
+			$this->assertEquals($user->getUsername(), "assertUser");
+			$this->assertEquals($user->getPassword(), "32cdb619196200050ab0af581a10fb83cfc63b1a20f58d4bafb6313d55a3f0e9");
+			$this->assertEquals($user->getEmail(), "assertUser@infected.no");
+			$this->assertEquals($user->getBirthdate(), 890953200);
+			$this->assertEquals($user->getGenderAsString(), "Gutt");
+			$this->assertEquals($user->getPhoneAsString(), "12 34 56 78");
+			$this->assertEquals($user->getPhone(), 12345678);
+			$this->assertEquals($user->getAddress(), "Test address");
+			$this->assertEquals($user->getPostalCode(), "1337");
+			$this->assertEquals($user->getNickname(), "AssertNick");
 		}
 
 		//One last thing, check if girl string also works
@@ -91,12 +82,12 @@ class UserTestSuite extends TestSuite {
 											   1337, 
 											   "AssertGirl");
 		
-		if ($this->assert_not_equals($createdUser, null)) {
+		if ($this->assertNotEquals($createdUser, null)) {
 			$user = UserHandler::getUserByIdentifier("assertGirl");
 
-			$this->assert_not_equals($user, null);
+			$this->assertNotEquals($user, null);
 
-			$this->assert_equals($user->getGenderAsString(), "Jente");
+			$this->assertEquals($user->getGenderAsString(), "Jente");
 		}
 	}
 }
