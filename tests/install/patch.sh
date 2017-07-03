683d8a86c3e931e801054dff5b2875fc7caf15d3
Liam Svanåsbakken Crouch <spam@petterroea.com>
2017-07-03 20:48:23 +0200

    fixed some patching

--- tests/install/patch.sh
+++ tests/install/patch.sh
@@ -1,2 +1,2 @@
 echo "Patching configs"
-sed -i "s/\/home\/' . self::domain . '\/public_html\/api\//~\//g" settings.php
+sed -i "s/\/home\/' . self::domain . '\/public_html\/api\//\/home\/travis\/build\/InfectedLan\/InfectedAPI/g" settings.php
