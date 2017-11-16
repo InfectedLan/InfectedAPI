c8b031e76c5699404355a4bbc5c28e81c181a6f8
Liam Svanåsbakken Crouch <spam@petterroea.com>
2017-11-16 17:05:03 +0100

    Tests

--- handlers/nfccardhandler.php
+++ handlers/nfccardhandler.php
@@ -22,9 +22,25 @@ require_once 'settings.php';
 require_once 'database.php';
 require_once 'objects/nfcgate.php';
 require_once 'objects/event.php';
+require_once 'objects/users.php';
 require_once 'handlers/eventhandler.php';
 
 class NfcCardHandler {
+	/*
+	 * Registers a NFC card for a user
+	*/
+	public static function registerCard(User $user, Event $event, $nfcid) {
+		$database = Database::getConnection(Settings::db_name_infected);
+
+		$result = $database->query('INSERT INTO `' . Settings::db_table_infected_nfccards . '` (`userId`, `eventId`, `nfcId`)
+																VALUES (\'' . $user->getId() . '\',
+																				\'' . $event->getId() . '\', 
+																				\'' . $database->real_escape_string($nfcid) . '\');');
+
+
+		return self::getCard($database->insert_id);
+	}
+
 	/*
 	 * Returns the card with the given id.
 	 */
@@ -43,7 +59,7 @@ class NfcCardHandler {
 	public static function getCardsByEvent(Event $event) {
 		$database = Database::getConnection(Settings::db_name_infected);
 
-		$result = $database->query('SELECT * FROM `' . Settings::db_table_infected_nfccards . '` WHERE `eventId` = \'' . $event->getId() . '\';');
+		$result = $database->query('SELECT * FROM `' . Settings::db_table_infected_nfccards . '` WHERE `eventId` = \'' . $database->real_escape_string($event->getId()) . '\';');
 
 		$cardList = [];
 
@@ -55,10 +71,29 @@ class NfcCardHandler {
 	}
 
 	/*
+	 * Returns the NFC card given an user and an event
+	 */
+	public static function getCardByUserAndEvent(Event $event, User $user) {
+		$database = Database::getConnection(Settings::db_name_infected);
+
+		$result = $database->query('SELECT * FROM `'. Settings::db_table_infected_nfccards . '`
+																WHERE `eventId` = \'' . $database->real_escape_string($event->getId()) . '\' AND `userId` = \'' . $database->real_escape_string($user->getId()) . '\';');
+
+		return $result->fetch_object('NfcCard');
+	}
+
+	/*
+	 * Returns a user's NFC card for the current event
+	 */
+	public static function getCardByUserForCurrentEvent(User $user) {
+		return self::getCardByUserAndEvent(EventHandler::getCurrentEvent(), $user);
+	}
+
+	/*
 	 * Returns a list of all nfc cards for the current event
 	 */
 	public static function getCardsForCurrentEvent() {
-		return getGatesByEvent(EventHandler::getCurrentEvent());
+		return self::getGatesByEvent(EventHandler::getCurrentEvent());
 	}
 }
 ?>
---
+++ tests/tests/NfcCardTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is part of InfectedAPI.
+ *
+ * Copyright (C) 2015 Infected <http://infected.no/>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+use PHPUnit\Framework\TestCase;
+
+require_once 'handlers/nfccardhandler.php';
+require_once 'handlers/userhandler.php';
+require_once 'handlers/eventhandler.php';
+require_once 'database.php';
+
+/* 
+ * NfcCardTest
+ *
+ * Responsible for testing the functionality of the NfcCard class
+ *
+ */
+class NfcCardTest extends TestCase {
+	public function test() {
+		$this->cardRegistrationTest();
+		$this->getterTest();
+		$this->cleanup();
+	}
+
+	private function cardRegistrationTest() {
+		//Constants used by the test
+		$me = UserHandler::getUser(1);
+		$nfcid = "E004010203040506";
+
+		$cards = NfcCardHandler::getCardsByCurrentEvent();
+		$this->assertEquals(0, count($cards));
+
+	}
+
+	private function getterTest() {
+		//RegisterCard and getCardsByCurrentEvent
+		NfcCardHandler::registerCard($me,EventHandler::getCurrentEvent(), $nfcid);
+		$cards = NfcCardHandler::getCardsByCurrentEvent();
+		$this->assertEquals(1, count($cards));
+
+		$card = $cards[0];
+		$this->assertEquals($me, $card->getUser());
+		$this->assertEquals($nfcid, $card->getNfcId());
+
+		//getCard
+		$newCard = NfcCardHandler::getCard($card->getId());
+		$this->assertEquals($card, $newCard);
+
+		//getCardByUserAndEvent
+		$newCard = NfcCardHandler::getCardByUserAndEvent($me, EventHandler::getCurrentEvent());
+		$this->assertEquals($card, $newCard);
+
+		//getCardByUserForCurrentEvent
+		$newCard = NfcCardHandler::getCardByUserForCurrentEvent($me);
+		$this->assertEquals($card, $newCard);
+	}
+
+	private function cleanup() {
+
+	}
+}
+?>
