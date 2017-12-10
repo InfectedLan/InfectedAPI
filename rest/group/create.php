db5952059b7a12ca23726d80b901e784603bb98d
halvors <halvors@halvors.org>
2017-12-10 03:04:24 +0100

    Added REST API for adding, editing, deleting groups.

---
+++ rest/group/create.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of InfectedAPI.
+ *
+ * Copyright (C) 2017 Infected <http://infected.no/>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'session.php';
+require_once 'database.php';
+require_once 'localization.php';
+require_once 'handlers/userhandler.php';
+require_once 'handlers/grouphandler.php';
+
+$result = false;
+$status = http_response_code();
+$message = null;
+
+if (Session::isAuthenticated()) {
+	$user = Session::getCurrentUser();
+
+	if ($user->hasPermission('chief.group')) {
+		if (isset($_POST['title']) &&
+			isset($_POST['description']) &&
+			isset($_POST['leaderId']) &&
+			!empty($_POST['title']) &&
+			!empty($_POST['description']) &&
+            is_numeric($_POST['leaderId'])) {
+			$name = strtolower(str_replace(' ', '-', $_POST['title']));
+			$title = ucfirst($_POST['title']);
+			$description = $_POST['description'];
+			$leaderUser = UserHandler::getUser($_POST['leaderId']);
+			
+			GroupHandler::createGroup($name, $title, $description, $leaderUser);
+			$result = true;
+            $status = 201; // Created.
+		} else {
+            $status = 400; // Bad Request.
+			$message = Localization::getLocale('you_have_not_filled_out_the_required_fields');
+		}
+	} else {
+		$message = Localization::getLocale('you_do_not_have_permission_to_do_that');
+	}
+} else {
+	$message = Localization::getLocale('you_are_not_logged_in');
+}
+
+http_response_code($status);
+header('Content-Type: application/json');
+echo json_encode(['result' => $result, 'message' => $message], JSON_PRETTY_PRINT);
+Database::cleanup();
