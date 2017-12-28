f7ee5d5b5de512eb4054b58c02c7f355b5daa114
halvors <halvors@halvors.org>
2017-12-28 03:54:00 +0100

    Fixed createUser() in UserHandler expecting boolean.

--- handlers/userhandler.php
+++ handlers/userhandler.php
@@ -234,7 +234,7 @@ class UserHandler {
 	 * Create a new user
 	 * TEST FIXERS NOTE: Gender can't be boolean. It will give an mysql error, as mysql expects an integer.
 	 */
-	public static function createUser(string $firstname, string $lastname, string $username, string $password, string $email, string $birthDate, bool $gender, int $phone, string $address, int $postalCode, string $nickname): User {
+	public static function createUser(string $firstname, string $lastname, string $username, string $password, string $email, string $birthDate, int $gender, int $phone, string $address, int $postalCode, string $nickname): User {
 		$database = Database::getConnection(Settings::db_name_infected);
 
 		$database->query('INSERT INTO `' . Settings::db_table_infected_users . '` (`firstname`, `lastname`, `username`, `password`, `email`, `birthdate`, `gender`, `phone`, `address`, `postalcode`, `countryId`, `nickname`, `registerdate`)
