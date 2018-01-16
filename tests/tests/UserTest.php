3ce8acbc909ed3870b6ac97794570f5437e6764b
Liam Svanåsbakken Crouch <spam@petterroea.com>
2018-01-16 01:48:04 +0100

    didnt know how the db looked sorry

--- tests/tests/UserTest.php
+++ tests/tests/UserTest.php
@@ -109,6 +109,17 @@ class UserTest extends TestCase {
 		$user1 = UserHandler::getUser(1);
 		$user2 = UserHandler::getUser(2);
 
+		$this->assertEquals(true, $user1->isFriendsWith($user2));
+		$this->assertEquals(true, $user2->isFriendsWith($user1));
+
+		$this->assertEquals(0, count($user1->getFriends()));
+		$this->assertEquals(0, count($user2->getFriends()));
+
+		$this->assertEquals(0, count($user1->getPendingFriends()));
+		$this->assertEquals(0, count($user2->getPendingFriends()));
+
+		$user1->removeFriend($user2);
+
 		$this->assertEquals(false, $user1->isFriendsWith($user2));
 		$this->assertEquals(false, $user2->isFriendsWith($user1));
 
