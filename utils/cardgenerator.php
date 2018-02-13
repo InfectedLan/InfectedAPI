0374fd1832298ec6b78fa28f287728b2a8f6315d
Liam Svanåsbakken Crouch <spam@petterroea.com>
2018-02-13 22:26:50 +0100

    started work on card generator

---
+++ utils/cardgenerator.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * This file is part of InfectedAPI.
+ *
+ * Copyright (C) 2018 Infected <https://infected.no/>.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3.0 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once 'handlers/userhandler.php';
+require_once 'objects/user.php';
+
+class CardGenerator {
+    const HEIGHT = 1920;
+    const WIDTH = HEIGHT*
+    public static function generateCard(User $user) {
+
+    }
+}
+
+?>
